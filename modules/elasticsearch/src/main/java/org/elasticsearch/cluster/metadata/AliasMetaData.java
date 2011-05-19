elastic/elasticsearch

Add an ability to define and store filter for aliases.
This commit only adds an ability to store filter source. The stored filters are not yet used to filter search results.

modules/elasticsearch/src/main/java/org/elasticsearch/cluster/metadata/AliasMetaData.java
@@ -19,12 +19,16 @@
 
 package org.elasticsearch.cluster.metadata;
 
+import org.elasticsearch.ElasticSearchGenerationException;
+import org.elasticsearch.common.Strings;
+import org.elasticsearch.common.compress.CompressedString;
 import org.elasticsearch.common.io.stream.StreamInput;
 import org.elasticsearch.common.io.stream.StreamOutput;
 import org.elasticsearch.common.util.concurrent.Immutable;
 import org.elasticsearch.common.xcontent.*;
 
 import java.io.IOException;
+import java.util.Map;
 
 /**
  * @author imotov
@@ -34,8 +38,11 @@ public class AliasMetaData {
 
     private final String alias;
 
-    private AliasMetaData(String alias) {
+    private final CompressedString source;
+
+    private AliasMetaData(String alias, CompressedString source) {
         this.alias = alias;
+        this.source = source;
     }
 
     public String alias() {
@@ -46,6 +53,14 @@ public class AliasMetaData {
         return alias();
     }
 
+    public CompressedString source() {
+        return source;
+    }
+
+    public CompressedString getSource() {
+        return source();
+    }
+
     public static Builder newAliasMetaDataBuilder(String alias) {
         return new Builder(alias);
     }
@@ -53,6 +68,8 @@ public class AliasMetaData {
     public static class Builder {
 
         private String alias;
+
+        private CompressedString source;
 
         public Builder(String alias) {
             this.alias = alias;
@@ -66,32 +83,102 @@ public class AliasMetaData {
             return alias;
         }
 
+        public Builder source(String source) {
+            if (!Strings.hasLength(source)) {
+                source = null;
+                return this;
+            }
+            try {
+                XContentParser parser = XContentFactory.xContent(source).createParser(source);
+                try {
+                    source(parser.map());
+                } finally {
+                    parser.close();
+                }
+                return this;
+            } catch (IOException e) {
+                throw new ElasticSearchGenerationException("Failed to generate [" + source + "]", e);
+            }
+        }
+
+        public Builder source(Map<String, Object> source) {
+            if (source == null || source.isEmpty()) {
+                source = null;
+                return this;
+            }
+            try {
+                this.source = new CompressedString(XContentFactory.jsonBuilder().map(source).string());
+                return this;
+            } catch (IOException e) {
+                throw new ElasticSearchGenerationException("Failed to generate [" + source + "]", e);
+            }
+        }
+
+        public Builder source(XContentBuilder sourceBuilder) {
+            try {
+                return source(sourceBuilder.string());
+            } catch (IOException e) {
+                throw new ElasticSearchGenerationException("Failed to build json for alias request", e);
+            }
+        }
+
         public AliasMetaData build() {
-            return new AliasMetaData(alias);
+            return new AliasMetaData(alias, source);
         }
 
         public static void toXContent(AliasMetaData aliasMetaData, XContentBuilder builder, ToXContent.Params params) throws IOException {
             builder.startObject(aliasMetaData.alias(), XContentBuilder.FieldCaseConversion.NONE);
-            // Filters will go here
+
+            if (aliasMetaData.source() != null) {
+                byte[] data = aliasMetaData.source().uncompressed();
+                XContentParser parser = XContentFactory.xContent(data).createParser(data);
+                Map<String, Object> mapping = parser.mapOrdered();
+                parser.close();
+                builder.field("source", mapping);
+            }
+
             builder.endObject();
         }
 
         public static AliasMetaData fromXContent(XContentParser parser) throws IOException {
             Builder builder = new Builder(parser.currentName());
+
+            String currentFieldName = null;
             XContentParser.Token token = parser.nextToken();
+            if (token == null) {
+                // no data...
+                return builder.build();
+            }
             while ((token = parser.nextToken()) != XContentParser.Token.END_OBJECT) {
-                // Skip the content for now, filter and other settings will go here
+                if (token == XContentParser.Token.FIELD_NAME) {
+                    currentFieldName = parser.currentName();
+                } else if (token == XContentParser.Token.START_OBJECT) {
+                    if ("source".equals(currentFieldName)) {
+                        Map<String, Object> mapping = parser.mapOrdered();
+                        builder.source(mapping);
+                    }
+                }
             }
             return builder.build();
         }
 
         public static void writeTo(AliasMetaData aliasMetaData, StreamOutput out) throws IOException {
             out.writeUTF(aliasMetaData.alias());
+            if (aliasMetaData.source() != null) {
+                out.writeBoolean(true);
+                aliasMetaData.source.writeTo(out);
+            } else {
+                out.writeBoolean(false);
+            }
         }
 
         public static AliasMetaData readFrom(StreamInput in) throws IOException {
             String alias = in.readUTF();
-            return new AliasMetaData(alias);
+            CompressedString source = null;
+            if (in.readBoolean()) {
+                source = CompressedString.readCompressedString(in);
+            }
+            return new AliasMetaData(alias, source);
         }
     }
 

modules/elasticsearch/src/main/java/org/elasticsearch/cluster/metadata/MetaDataIndexAliasesService.java
@@ -68,7 +68,7 @@ public class MetaDataIndexAliasesService extends AbstractComponent {
                     }
                     IndexMetaData.Builder indexMetaDataBuilder = newIndexMetaDataBuilder(indexMetaData);
                     if (aliasAction.actionType() == AliasAction.Type.ADD) {
-                        indexMetaDataBuilder.putAlias(AliasMetaData.newAliasMetaDataBuilder(aliasAction.alias()).build());
+                        indexMetaDataBuilder.putAlias(AliasMetaData.newAliasMetaDataBuilder(aliasAction.alias()).source(aliasAction.source()).build());
                     } else if (aliasAction.actionType() == AliasAction.Type.REMOVE) {
                         indexMetaDataBuilder.removerAlias(aliasAction.alias());
                     }

modules/elasticsearch/src/test/java/org/elasticsearch/cluster/metadata/ToAndFromJsonMetaDataTests.java
@@ -78,6 +78,19 @@ public class ToAndFromJsonMetaDataTests {
                         .putMapping("mapping2", MAPPING_SOURCE2)
                         .putAlias(newAliasMetaDataBuilder("alias1"))
                         .putAlias(newAliasMetaDataBuilder("alias2")))
+                .put(newIndexMetaDataBuilder("test7")
+                        .settings(settingsBuilder()
+                                .put("setting1", "value1")
+                                .put("setting2", "value2")
+                                .put("index.aliases.0", "alias3")
+                                .put("index.aliases.1", "alias1"))
+                        .numberOfShards(1)
+                        .numberOfReplicas(2)
+                        .putMapping("mapping1", MAPPING_SOURCE1)
+                        .putMapping("mapping2", MAPPING_SOURCE2)
+                        .putAlias(newAliasMetaDataBuilder("alias1").source(ALIAS_SOURCE1))
+                        .putAlias(newAliasMetaDataBuilder("alias2"))
+                        .putAlias(newAliasMetaDataBuilder("alias4").source(ALIAS_SOURCE2)))
                 .build();
 
         String metaDataSource = MetaData.Builder.toXContent(metaData);
@@ -142,8 +155,29 @@ public class ToAndFromJsonMetaDataTests {
         assertThat(indexMetaData.aliases().get("alias1").alias(), equalTo("alias1"));
         assertThat(indexMetaData.aliases().get("alias2").alias(), equalTo("alias2"));
         assertThat(indexMetaData.aliases().get("alias3").alias(), equalTo("alias3"));
+
+        indexMetaData = parsedMetaData.index("test7");
+        assertThat(indexMetaData.numberOfShards(), equalTo(1));
+        assertThat(indexMetaData.numberOfReplicas(), equalTo(2));
+        assertThat(indexMetaData.settings().getAsMap().size(), equalTo(4));
+        assertThat(indexMetaData.settings().get("setting1"), equalTo("value1"));
+        assertThat(indexMetaData.settings().get("setting2"), equalTo("value2"));
+        assertThat(indexMetaData.mappings().size(), equalTo(2));
+        assertThat(indexMetaData.mappings().get("mapping1").source().string(), equalTo(MAPPING_SOURCE1));
+        assertThat(indexMetaData.mappings().get("mapping2").source().string(), equalTo(MAPPING_SOURCE2));
+        assertThat(indexMetaData.aliases().size(), equalTo(4));
+        assertThat(indexMetaData.aliases().get("alias1").alias(), equalTo("alias1"));
+        assertThat(indexMetaData.aliases().get("alias1").source().string(), equalTo(ALIAS_SOURCE1));
+        assertThat(indexMetaData.aliases().get("alias2").alias(), equalTo("alias2"));
+        assertThat(indexMetaData.aliases().get("alias2").source(), nullValue());
+        assertThat(indexMetaData.aliases().get("alias3").alias(), equalTo("alias3"));
+        assertThat(indexMetaData.aliases().get("alias3").source(), nullValue());
+        assertThat(indexMetaData.aliases().get("alias4").alias(), equalTo("alias4"));
+        assertThat(indexMetaData.aliases().get("alias4").source().string(), equalTo(ALIAS_SOURCE2));
     }
 
     private static final String MAPPING_SOURCE1 = "{\"mapping1\":{\"text1\":{\"type\":\"string\"}}}";
     private static final String MAPPING_SOURCE2 = "{\"mapping2\":{\"text2\":{\"type\":\"string\"}}}";
+    private static final String ALIAS_SOURCE1 = "{\"field1\":\"value1\"}";
+    private static final String ALIAS_SOURCE2 = "{\"field2\":\"value2\"}";
 }

openeuler-mirror/poissonsearch-oss

modules/elasticsearch/src/main/java/org/elasticsearch/cluster/metadata/AliasMetaData.java
@@ -19,12 +19,16 @@
 
 package org.elasticsearch.cluster.metadata;
 
+import org.elasticsearch.ElasticSearchGenerationException;
+import org.elasticsearch.common.Strings;
+import org.elasticsearch.common.compress.CompressedString;
 import org.elasticsearch.common.io.stream.StreamInput;
 import org.elasticsearch.common.io.stream.StreamOutput;
 import org.elasticsearch.common.util.concurrent.Immutable;
 import org.elasticsearch.common.xcontent.*;
 
 import java.io.IOException;
+import java.util.Map;
 
 /**
  * @author imotov
@@ -34,8 +38,11 @@ public class AliasMetaData {
 
     private final String alias;
 
-    private AliasMetaData(String alias) {
+    private final CompressedString filter;
+
+    private AliasMetaData(String alias, CompressedString filter) {
         this.alias = alias;
+        this.filter = filter;
     }
 
     public String alias() {
@@ -46,6 +53,14 @@ public class AliasMetaData {
         return alias();
     }
 
+    public CompressedString filter() {
+        return filter;
+    }
+
+    public CompressedString getFilter() {
+        return filter();
+    }
+
     public static Builder newAliasMetaDataBuilder(String alias) {
         return new Builder(alias);
     }
@@ -53,6 +68,8 @@ public class AliasMetaData {
     public static class Builder {
 
         private String alias;
+
+        private CompressedString filter;
 
         public Builder(String alias) {
             this.alias = alias;
@@ -66,32 +83,102 @@ public class AliasMetaData {
             return alias;
         }
 
+        public Builder filter(String filter) {
+            if (!Strings.hasLength(filter)) {
+                this.filter = null;
+                return this;
+            }
+            try {
+                XContentParser parser = XContentFactory.xContent(filter).createParser(filter);
+                try {
+                    filter(parser.mapOrdered());
+                } finally {
+                    parser.close();
+                }
+                return this;
+            } catch (IOException e) {
+                throw new ElasticSearchGenerationException("Failed to generate [" + filter + "]", e);
+            }
+        }
+
+        public Builder filter(Map<String, Object> filter) {
+            if (filter == null || filter.isEmpty()) {
+                this.filter = null;
+                return this;
+            }
+            try {
+                this.filter = new CompressedString(XContentFactory.jsonBuilder().map(filter).string());
+                return this;
+            } catch (IOException e) {
+                throw new ElasticSearchGenerationException("Failed to build json for alias request", e);
+            }
+        }
+
+        public Builder filter(XContentBuilder filterBuilder) {
+            try {
+                return filter(filterBuilder.string());
+            } catch (IOException e) {
+                throw new ElasticSearchGenerationException("Failed to build json for alias request", e);
+            }
+        }
+
         public AliasMetaData build() {
-            return new AliasMetaData(alias);
+            return new AliasMetaData(alias, filter);
         }
 
         public static void toXContent(AliasMetaData aliasMetaData, XContentBuilder builder, ToXContent.Params params) throws IOException {
             builder.startObject(aliasMetaData.alias(), XContentBuilder.FieldCaseConversion.NONE);
-            // Filters will go here
+
+            if (aliasMetaData.filter() != null) {
+                byte[] data = aliasMetaData.filter().uncompressed();
+                XContentParser parser = XContentFactory.xContent(data).createParser(data);
+                Map<String, Object> filter = parser.mapOrdered();
+                parser.close();
+                builder.field("filter", filter);
+            }
+
             builder.endObject();
         }
 
         public static AliasMetaData fromXContent(XContentParser parser) throws IOException {
             Builder builder = new Builder(parser.currentName());
+
+            String currentFieldName = null;
             XContentParser.Token token = parser.nextToken();
+            if (token == null) {
+                // no data...
+                return builder.build();
+            }
             while ((token = parser.nextToken()) != XContentParser.Token.END_OBJECT) {
-                // Skip the content for now, filter and other settings will go here
+                if (token == XContentParser.Token.FIELD_NAME) {
+                    currentFieldName = parser.currentName();
+                } else if (token == XContentParser.Token.START_OBJECT) {
+                    if ("filter".equals(currentFieldName)) {
+                        Map<String, Object> filter = parser.mapOrdered();
+                        builder.filter(filter);
+                    }
+                }
             }
             return builder.build();
         }
 
         public static void writeTo(AliasMetaData aliasMetaData, StreamOutput out) throws IOException {
             out.writeUTF(aliasMetaData.alias());
+            if (aliasMetaData.filter() != null) {
+                out.writeBoolean(true);
+                aliasMetaData.filter.writeTo(out);
+            } else {
+                out.writeBoolean(false);
+            }
         }
 
         public static AliasMetaData readFrom(StreamInput in) throws IOException {
             String alias = in.readUTF();
-            return new AliasMetaData(alias);
+            CompressedString filter = null;
+            if (in.readBoolean()) {
+                filter = CompressedString.readCompressedString(in);
+            }
+            return new AliasMetaData(alias, filter);
         }
     }
 

modules/elasticsearch/src/main/java/org/elasticsearch/cluster/metadata/MetaDataIndexAliasesService.java
@@ -68,7 +68,7 @@ public class MetaDataIndexAliasesService extends AbstractComponent {
                     }
                     IndexMetaData.Builder indexMetaDataBuilder = newIndexMetaDataBuilder(indexMetaData);
                     if (aliasAction.actionType() == AliasAction.Type.ADD) {
-                        indexMetaDataBuilder.putAlias(AliasMetaData.newAliasMetaDataBuilder(aliasAction.alias()).build());
+                        indexMetaDataBuilder.putAlias(AliasMetaData.newAliasMetaDataBuilder(aliasAction.alias()).filter(aliasAction.filter()).build());
                     } else if (aliasAction.actionType() == AliasAction.Type.REMOVE) {
                         indexMetaDataBuilder.removerAlias(aliasAction.alias());
                     }

modules/elasticsearch/src/test/java/org/elasticsearch/cluster/metadata/ToAndFromJsonMetaDataTests.java
@@ -78,6 +78,19 @@ public class ToAndFromJsonMetaDataTests {
                         .putMapping("mapping2", MAPPING_SOURCE2)
                         .putAlias(newAliasMetaDataBuilder("alias1"))
                         .putAlias(newAliasMetaDataBuilder("alias2")))
+                .put(newIndexMetaDataBuilder("test7")
+                        .settings(settingsBuilder()
+                                .put("setting1", "value1")
+                                .put("setting2", "value2")
+                                .put("index.aliases.0", "alias3")
+                                .put("index.aliases.1", "alias1"))
+                        .numberOfShards(1)
+                        .numberOfReplicas(2)
+                        .putMapping("mapping1", MAPPING_SOURCE1)
+                        .putMapping("mapping2", MAPPING_SOURCE2)
+                        .putAlias(newAliasMetaDataBuilder("alias1").filter(ALIAS_FILTER1))
+                        .putAlias(newAliasMetaDataBuilder("alias2"))
+                        .putAlias(newAliasMetaDataBuilder("alias4").filter(ALIAS_FILTER2)))
                 .build();
 
         String metaDataSource = MetaData.Builder.toXContent(metaData);
@@ -142,8 +155,29 @@ public class ToAndFromJsonMetaDataTests {
         assertThat(indexMetaData.aliases().get("alias1").alias(), equalTo("alias1"));
         assertThat(indexMetaData.aliases().get("alias2").alias(), equalTo("alias2"));
         assertThat(indexMetaData.aliases().get("alias3").alias(), equalTo("alias3"));
+
+        indexMetaData = parsedMetaData.index("test7");
+        assertThat(indexMetaData.numberOfShards(), equalTo(1));
+        assertThat(indexMetaData.numberOfReplicas(), equalTo(2));
+        assertThat(indexMetaData.settings().getAsMap().size(), equalTo(4));
+        assertThat(indexMetaData.settings().get("setting1"), equalTo("value1"));
+        assertThat(indexMetaData.settings().get("setting2"), equalTo("value2"));
+        assertThat(indexMetaData.mappings().size(), equalTo(2));
+        assertThat(indexMetaData.mappings().get("mapping1").source().string(), equalTo(MAPPING_SOURCE1));
+        assertThat(indexMetaData.mappings().get("mapping2").source().string(), equalTo(MAPPING_SOURCE2));
+        assertThat(indexMetaData.aliases().size(), equalTo(4));
+        assertThat(indexMetaData.aliases().get("alias1").alias(), equalTo("alias1"));
+        assertThat(indexMetaData.aliases().get("alias1").filter().string(), equalTo(ALIAS_FILTER1));
+        assertThat(indexMetaData.aliases().get("alias2").alias(), equalTo("alias2"));
+        assertThat(indexMetaData.aliases().get("alias2").filter(), nullValue());
+        assertThat(indexMetaData.aliases().get("alias3").alias(), equalTo("alias3"));
+        assertThat(indexMetaData.aliases().get("alias3").filter(), nullValue());
+        assertThat(indexMetaData.aliases().get("alias4").alias(), equalTo("alias4"));
+        assertThat(indexMetaData.aliases().get("alias4").filter().string(), equalTo(ALIAS_FILTER2));
     }
 
     private static final String MAPPING_SOURCE1 = "{\"mapping1\":{\"text1\":{\"type\":\"string\"}}}";
     private static final String MAPPING_SOURCE2 = "{\"mapping2\":{\"text2\":{\"type\":\"string\"}}}";
+    private static final String ALIAS_FILTER1 = "{\"field1\":\"value1\"}";
+    private static final String ALIAS_FILTER2 = "{\"field2\":\"value2\"}";
 }