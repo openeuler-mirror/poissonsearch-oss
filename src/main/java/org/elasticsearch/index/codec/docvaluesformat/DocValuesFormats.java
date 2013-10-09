elastic/elasticsearch

Doc values integration.
This commit allows for using Lucene doc values as a backend for field data,
moving the cost of building field data from the refresh operation to indexing.
In addition, Lucene doc values can be stored on disk (partially, or even
entirely), so that memory management is done at the operating system level
(file-system cache) instead of the JVM, avoiding long pauses during major
collections due to large heaps.

So far doc values are supported on numeric types and non-analyzed strings
(index:no or index:not_analyzed). Under the hood, it uses SORTED_SET doc values
which is the only type to support multi-valued fields. Since the field data API
set is a bit wider than the doc values API set, some operations are not
supported:
 - field data filtering: this will fail if doc values are enabled,
 - field data cache clearing, even for memory-based doc values formats,
 - getting the memory usage for a specific field,
 - knowing whether a field is actually multi-valued.

This commit also allows for configuring doc-values formats on a per-field basis
similarly to postings formats. In particular the doc values format of the
_version field can be configured through its own field mapper (it used to be
handled in UidFieldMapper previously).

Closes #3806

src/main/java/org/elasticsearch/index/codec/docvaluesformat/DocValuesFormats.java
@@ -1,0 +1,62 @@
+/*
+ * Licensed to ElasticSearch and Shay Banon under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership. ElasticSearch licenses this
+ * file to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+package org.elasticsearch.index.codec.docvaluesformat;
+
+import com.google.common.collect.ImmutableCollection;
+import com.google.common.collect.ImmutableMap;
+import org.apache.lucene.codecs.DocValuesFormat;
+import org.apache.lucene.util.Version;
+import org.elasticsearch.common.collect.MapBuilder;
+import org.elasticsearch.common.lucene.Lucene;
+
+/**
+ * This class represents the set of Elasticsearch "built-in"
+ * {@link DocValuesFormatProvider.Factory doc values format factories}
+ */
+public class DocValuesFormats {
+
+    private static final ImmutableMap<String, PreBuiltDocValuesFormatProvider.Factory> builtInDocValuesFormats;
+
+    static {
+        assert Version.LUCENE_44.onOrAfter(Lucene.VERSION) : "change memory to point to the new Lucene 4.5 MemoryDVF";
+        MapBuilder<String, PreBuiltDocValuesFormatProvider.Factory> builtInDocValuesFormatsX = MapBuilder.newMapBuilder();
+        for (String name : DocValuesFormat.availableDocValuesFormats()) {
+            builtInDocValuesFormatsX.put(name, new PreBuiltDocValuesFormatProvider.Factory(DocValuesFormat.forName(name)));
+        }
+        // LUCENE UPGRADE: update those DVF if necessary
+        builtInDocValuesFormatsX.put("default", new PreBuiltDocValuesFormatProvider.Factory("default", DocValuesFormat.forName("Lucene42")));
+        builtInDocValuesFormatsX.put("memory", new PreBuiltDocValuesFormatProvider.Factory("memory", DocValuesFormat.forName("Lucene42")));
+        builtInDocValuesFormatsX.put("disk", new PreBuiltDocValuesFormatProvider.Factory("disk", DocValuesFormat.forName("Disk")));
+        builtInDocValuesFormats = builtInDocValuesFormatsX.immutableMap();
+    }
+
+    public static DocValuesFormatProvider.Factory getAsFactory(String name) {
+        return builtInDocValuesFormats.get(name);
+    }
+
+    public static DocValuesFormatProvider getAsProvider(String name) {
+        final PreBuiltDocValuesFormatProvider.Factory factory = builtInDocValuesFormats.get(name);
+        return factory == null ? null : factory.get();
+    }
+
+    public static ImmutableCollection<PreBuiltDocValuesFormatProvider.Factory> listFactories() {
+        return builtInDocValuesFormats.values();
+    }
+}

src/main/java/org/elasticsearch/index/fielddata/IndexFieldDataService.java
@@ -43,6 +43,7 @@ import java.util.concurrent.ConcurrentMap;
 public class IndexFieldDataService extends AbstractIndexComponent {
 
     private final static ImmutableMap<String, IndexFieldData.Builder> buildersByType;
+    private final static ImmutableMap<String, IndexFieldData.Builder> docValuesBuildersByType;
     private final static ImmutableMap<Tuple<String, String>, IndexFieldData.Builder> buildersByTypeAndFormat;
 
     static {
@@ -57,21 +58,38 @@ public class IndexFieldDataService extends AbstractIndexComponent {
                 .put("geo_point", new GeoPointDoubleArrayIndexFieldData.Builder())
                 .immutableMap();
 
+        docValuesBuildersByType = MapBuilder.<String, IndexFieldData.Builder>newMapBuilder()
+                .put("string", new DocValuesIndexFieldData.Builder())
+                .put("float", new DocValuesIndexFieldData.Builder().numericType(IndexNumericFieldData.NumericType.FLOAT))
+                .put("double", new DocValuesIndexFieldData.Builder().numericType(IndexNumericFieldData.NumericType.DOUBLE))
+                .put("byte", new DocValuesIndexFieldData.Builder().numericType(IndexNumericFieldData.NumericType.BYTE))
+                .put("short", new DocValuesIndexFieldData.Builder().numericType(IndexNumericFieldData.NumericType.SHORT))
+                .put("int", new DocValuesIndexFieldData.Builder().numericType(IndexNumericFieldData.NumericType.INT))
+                .put("long", new DocValuesIndexFieldData.Builder().numericType(IndexNumericFieldData.NumericType.LONG))
+                .immutableMap();
+
         buildersByTypeAndFormat = MapBuilder.<Tuple<String, String>, IndexFieldData.Builder>newMapBuilder()
                 .put(Tuple.tuple("string", "paged_bytes"), new PagedBytesIndexFieldData.Builder())
                 .put(Tuple.tuple("string", "fst"), new FSTBytesIndexFieldData.Builder())
+                .put(Tuple.tuple("string", "doc_values"), new DocValuesIndexFieldData.Builder())
                 .put(Tuple.tuple("float", "array"), new FloatArrayIndexFieldData.Builder())
+                .put(Tuple.tuple("float", "doc_values"), new DocValuesIndexFieldData.Builder().numericType(IndexNumericFieldData.NumericType.FLOAT))
                 .put(Tuple.tuple("double", "array"), new DoubleArrayIndexFieldData.Builder())
+                .put(Tuple.tuple("double", "doc_values"), new DocValuesIndexFieldData.Builder().numericType(IndexNumericFieldData.NumericType.DOUBLE))
                 .put(Tuple.tuple("byte", "array"), new PackedArrayIndexFieldData.Builder().setNumericType(IndexNumericFieldData.NumericType.BYTE))
+                .put(Tuple.tuple("byte", "doc_values"), new DocValuesIndexFieldData.Builder().numericType(IndexNumericFieldData.NumericType.BYTE))
                 .put(Tuple.tuple("short", "array"), new PackedArrayIndexFieldData.Builder().setNumericType(IndexNumericFieldData.NumericType.SHORT))
+                .put(Tuple.tuple("short", "doc_values"), new DocValuesIndexFieldData.Builder().numericType(IndexNumericFieldData.NumericType.SHORT))
                 .put(Tuple.tuple("int", "array"), new PackedArrayIndexFieldData.Builder().setNumericType(IndexNumericFieldData.NumericType.INT))
+                .put(Tuple.tuple("int", "doc_values"), new DocValuesIndexFieldData.Builder().numericType(IndexNumericFieldData.NumericType.INT))
                 .put(Tuple.tuple("long", "array"), new PackedArrayIndexFieldData.Builder().setNumericType(IndexNumericFieldData.NumericType.LONG))
+                .put(Tuple.tuple("long", "doc_values"), new DocValuesIndexFieldData.Builder().numericType(IndexNumericFieldData.NumericType.LONG))
                 .put(Tuple.tuple("geo_point", "array"), new GeoPointDoubleArrayIndexFieldData.Builder())
                 .immutableMap();
     }
 
     private final IndicesFieldDataCache indicesFieldDataCache;
-    private final ConcurrentMap<String, IndexFieldData> loadedFieldData = ConcurrentCollections.newConcurrentMap();
+    private final ConcurrentMap<String, IndexFieldData<?>> loadedFieldData = ConcurrentCollections.newConcurrentMap();
 
     IndexService indexService;
 
@@ -92,7 +110,7 @@ public class IndexFieldDataService extends AbstractIndexComponent {
 
     public void clear() {
         synchronized (loadedFieldData) {
-            for (IndexFieldData fieldData : loadedFieldData.values()) {
+            for (IndexFieldData<?> fieldData : loadedFieldData.values()) {
                 fieldData.clear();
             }
             loadedFieldData.clear();
@@ -101,7 +119,7 @@ public class IndexFieldDataService extends AbstractIndexComponent {
 
     public void clearField(String fieldName) {
         synchronized (loadedFieldData) {
-            IndexFieldData fieldData = loadedFieldData.remove(fieldName);
+            IndexFieldData<?> fieldData = loadedFieldData.remove(fieldName);
             if (fieldData != null) {
                 fieldData.clear();
             }
@@ -109,28 +127,35 @@ public class IndexFieldDataService extends AbstractIndexComponent {
     }
 
     public void clear(IndexReader reader) {
-        for (IndexFieldData indexFieldData : loadedFieldData.values()) {
+        for (IndexFieldData<?> indexFieldData : loadedFieldData.values()) {
             indexFieldData.clear(reader);
         }
     }
 
-    public <IFD extends IndexFieldData> IFD getForField(FieldMapper mapper) {
-        return getForField(mapper.names(), mapper.fieldDataType());
+    public <IFD extends IndexFieldData<?>> IFD getForField(FieldMapper<?> mapper) {
+        return getForField(mapper.names(), mapper.fieldDataType(), mapper.hasDocValues());
     }
 
-    public <IFD extends IndexFieldData> IFD getForField(FieldMapper.Names fieldNames, FieldDataType type) {
-        IndexFieldData fieldData = loadedFieldData.get(fieldNames.indexName());
+    public <IFD extends IndexFieldData<?>> IFD getForField(FieldMapper.Names fieldNames, FieldDataType type, boolean docValues) {
+        IndexFieldData<?> fieldData = loadedFieldData.get(fieldNames.indexName());
         if (fieldData == null) {
             synchronized (loadedFieldData) {
                 fieldData = loadedFieldData.get(fieldNames.indexName());
                 if (fieldData == null) {
                     IndexFieldData.Builder builder = null;
-                    String format = type.getSettings().get("format", indexSettings.get("index.fielddata.type." + type.getType() + ".format", null));
+                    String format = type.getFormat(indexSettings);
+                    if (format != null && FieldDataType.FORMAT_DOC_VALUES.equals(format) && !docValues) {
+                        logger.warn("field [" + fieldNames.fullName() + "] has no doc values, will use default field data format");
+                        format = null;
+                    }
                     if (format != null) {
                         builder = buildersByTypeAndFormat.get(Tuple.tuple(type.getType(), format));
                         if (builder == null) {
                             logger.warn("failed to find format [" + format + "] for field [" + fieldNames.fullName() + "], will use default");
                         }
+                    }
+                    if (builder == null && docValues) {
+                        builder = docValuesBuildersByType.get(type.getType());
                     }
                     if (builder == null) {
                         builder = buildersByType.get(type.getType());

openeuler-mirror/poissonsearch-oss

src/main/java/org/elasticsearch/index/codec/docvaluesformat/DocValuesFormats.java
@@ -1,0 +1,59 @@
+/*
+ * Licensed to ElasticSearch and Shay Banon under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership. ElasticSearch licenses this
+ * file to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+package org.elasticsearch.index.codec.docvaluesformat;
+
+import com.google.common.collect.ImmutableCollection;
+import com.google.common.collect.ImmutableMap;
+import org.apache.lucene.codecs.DocValuesFormat;
+import org.elasticsearch.common.collect.MapBuilder;
+
+/**
+ * This class represents the set of Elasticsearch "built-in"
+ * {@link DocValuesFormatProvider.Factory doc values format factories}
+ */
+public class DocValuesFormats {
+
+    private static final ImmutableMap<String, PreBuiltDocValuesFormatProvider.Factory> builtInDocValuesFormats;
+
+    static {
+        MapBuilder<String, PreBuiltDocValuesFormatProvider.Factory> builtInDocValuesFormatsX = MapBuilder.newMapBuilder();
+        for (String name : DocValuesFormat.availableDocValuesFormats()) {
+            builtInDocValuesFormatsX.put(name, new PreBuiltDocValuesFormatProvider.Factory(DocValuesFormat.forName(name)));
+        }
+        // LUCENE UPGRADE: update those DVF if necessary
+        builtInDocValuesFormatsX.put("default", new PreBuiltDocValuesFormatProvider.Factory("default", DocValuesFormat.forName("Lucene45")));
+        builtInDocValuesFormatsX.put("memory", new PreBuiltDocValuesFormatProvider.Factory("memory", DocValuesFormat.forName("Memory")));
+        builtInDocValuesFormatsX.put("disk", new PreBuiltDocValuesFormatProvider.Factory("disk", DocValuesFormat.forName("Disk")));
+        builtInDocValuesFormats = builtInDocValuesFormatsX.immutableMap();
+    }
+
+    public static DocValuesFormatProvider.Factory getAsFactory(String name) {
+        return builtInDocValuesFormats.get(name);
+    }
+
+    public static DocValuesFormatProvider getAsProvider(String name) {
+        final PreBuiltDocValuesFormatProvider.Factory factory = builtInDocValuesFormats.get(name);
+        return factory == null ? null : factory.get();
+    }
+
+    public static ImmutableCollection<PreBuiltDocValuesFormatProvider.Factory> listFactories() {
+        return builtInDocValuesFormats.values();
+    }
+}

src/main/java/org/elasticsearch/index/fielddata/IndexFieldDataService.java
@@ -43,6 +43,7 @@ import java.util.concurrent.ConcurrentMap;
 public class IndexFieldDataService extends AbstractIndexComponent {
 
     private final static ImmutableMap<String, IndexFieldData.Builder> buildersByType;
+    private final static ImmutableMap<String, IndexFieldData.Builder> docValuesBuildersByType;
     private final static ImmutableMap<Tuple<String, String>, IndexFieldData.Builder> buildersByTypeAndFormat;
 
     static {
@@ -57,21 +58,38 @@ public class IndexFieldDataService extends AbstractIndexComponent {
                 .put("geo_point", new GeoPointDoubleArrayIndexFieldData.Builder())
                 .immutableMap();
 
+        docValuesBuildersByType = MapBuilder.<String, IndexFieldData.Builder>newMapBuilder()
+                .put("string", new DocValuesIndexFieldData.Builder())
+                .put("float", new DocValuesIndexFieldData.Builder().numericType(IndexNumericFieldData.NumericType.FLOAT))
+                .put("double", new DocValuesIndexFieldData.Builder().numericType(IndexNumericFieldData.NumericType.DOUBLE))
+                .put("byte", new DocValuesIndexFieldData.Builder().numericType(IndexNumericFieldData.NumericType.BYTE))
+                .put("short", new DocValuesIndexFieldData.Builder().numericType(IndexNumericFieldData.NumericType.SHORT))
+                .put("int", new DocValuesIndexFieldData.Builder().numericType(IndexNumericFieldData.NumericType.INT))
+                .put("long", new DocValuesIndexFieldData.Builder().numericType(IndexNumericFieldData.NumericType.LONG))
+                .immutableMap();
+
         buildersByTypeAndFormat = MapBuilder.<Tuple<String, String>, IndexFieldData.Builder>newMapBuilder()
                 .put(Tuple.tuple("string", "paged_bytes"), new PagedBytesIndexFieldData.Builder())
                 .put(Tuple.tuple("string", "fst"), new FSTBytesIndexFieldData.Builder())
+                .put(Tuple.tuple("string", "doc_values"), new DocValuesIndexFieldData.Builder())
                 .put(Tuple.tuple("float", "array"), new FloatArrayIndexFieldData.Builder())
+                .put(Tuple.tuple("float", "doc_values"), new DocValuesIndexFieldData.Builder().numericType(IndexNumericFieldData.NumericType.FLOAT))
                 .put(Tuple.tuple("double", "array"), new DoubleArrayIndexFieldData.Builder())
+                .put(Tuple.tuple("double", "doc_values"), new DocValuesIndexFieldData.Builder().numericType(IndexNumericFieldData.NumericType.DOUBLE))
                 .put(Tuple.tuple("byte", "array"), new PackedArrayIndexFieldData.Builder().setNumericType(IndexNumericFieldData.NumericType.BYTE))
+                .put(Tuple.tuple("byte", "doc_values"), new DocValuesIndexFieldData.Builder().numericType(IndexNumericFieldData.NumericType.BYTE))
                 .put(Tuple.tuple("short", "array"), new PackedArrayIndexFieldData.Builder().setNumericType(IndexNumericFieldData.NumericType.SHORT))
+                .put(Tuple.tuple("short", "doc_values"), new DocValuesIndexFieldData.Builder().numericType(IndexNumericFieldData.NumericType.SHORT))
                 .put(Tuple.tuple("int", "array"), new PackedArrayIndexFieldData.Builder().setNumericType(IndexNumericFieldData.NumericType.INT))
+                .put(Tuple.tuple("int", "doc_values"), new DocValuesIndexFieldData.Builder().numericType(IndexNumericFieldData.NumericType.INT))
                 .put(Tuple.tuple("long", "array"), new PackedArrayIndexFieldData.Builder().setNumericType(IndexNumericFieldData.NumericType.LONG))
+                .put(Tuple.tuple("long", "doc_values"), new DocValuesIndexFieldData.Builder().numericType(IndexNumericFieldData.NumericType.LONG))
                 .put(Tuple.tuple("geo_point", "array"), new GeoPointDoubleArrayIndexFieldData.Builder())
                 .immutableMap();
     }
 
     private final IndicesFieldDataCache indicesFieldDataCache;
-    private final ConcurrentMap<String, IndexFieldData> loadedFieldData = ConcurrentCollections.newConcurrentMap();
+    private final ConcurrentMap<String, IndexFieldData<?>> loadedFieldData = ConcurrentCollections.newConcurrentMap();
 
     IndexService indexService;
 
@@ -92,7 +110,7 @@ public class IndexFieldDataService extends AbstractIndexComponent {
 
     public void clear() {
         synchronized (loadedFieldData) {
-            for (IndexFieldData fieldData : loadedFieldData.values()) {
+            for (IndexFieldData<?> fieldData : loadedFieldData.values()) {
                 fieldData.clear();
             }
             loadedFieldData.clear();
@@ -101,7 +119,7 @@ public class IndexFieldDataService extends AbstractIndexComponent {
 
     public void clearField(String fieldName) {
         synchronized (loadedFieldData) {
-            IndexFieldData fieldData = loadedFieldData.remove(fieldName);
+            IndexFieldData<?> fieldData = loadedFieldData.remove(fieldName);
             if (fieldData != null) {
                 fieldData.clear();
             }
@@ -109,28 +127,35 @@ public class IndexFieldDataService extends AbstractIndexComponent {
     }
 
     public void clear(IndexReader reader) {
-        for (IndexFieldData indexFieldData : loadedFieldData.values()) {
+        for (IndexFieldData<?> indexFieldData : loadedFieldData.values()) {
             indexFieldData.clear(reader);
         }
     }
 
-    public <IFD extends IndexFieldData> IFD getForField(FieldMapper mapper) {
-        return getForField(mapper.names(), mapper.fieldDataType());
+    public <IFD extends IndexFieldData<?>> IFD getForField(FieldMapper<?> mapper) {
+        return getForField(mapper.names(), mapper.fieldDataType(), mapper.hasDocValues());
     }
 
-    public <IFD extends IndexFieldData> IFD getForField(FieldMapper.Names fieldNames, FieldDataType type) {
-        IndexFieldData fieldData = loadedFieldData.get(fieldNames.indexName());
+    public <IFD extends IndexFieldData<?>> IFD getForField(FieldMapper.Names fieldNames, FieldDataType type, boolean docValues) {
+        IndexFieldData<?> fieldData = loadedFieldData.get(fieldNames.indexName());
         if (fieldData == null) {
             synchronized (loadedFieldData) {
                 fieldData = loadedFieldData.get(fieldNames.indexName());
                 if (fieldData == null) {
                     IndexFieldData.Builder builder = null;
-                    String format = type.getSettings().get("format", indexSettings.get("index.fielddata.type." + type.getType() + ".format", null));
+                    String format = type.getFormat(indexSettings);
+                    if (format != null && FieldDataType.DOC_VALUES_FORMAT_VALUE.equals(format) && !docValues) {
+                        logger.warn("field [" + fieldNames.fullName() + "] has no doc values, will use default field data format");
+                        format = null;
+                    }
                     if (format != null) {
                         builder = buildersByTypeAndFormat.get(Tuple.tuple(type.getType(), format));
                         if (builder == null) {
                             logger.warn("failed to find format [" + format + "] for field [" + fieldNames.fullName() + "], will use default");
                         }
+                    }
+                    if (builder == null && docValues) {
+                        builder = docValuesBuildersByType.get(type.getType());
                     }
                     if (builder == null) {
                         builder = buildersByType.get(type.getType());