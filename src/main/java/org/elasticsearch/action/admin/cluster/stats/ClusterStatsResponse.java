elastic/elasticsearch

Added Cluster Stats API
Closes #4460

src/main/java/org/elasticsearch/action/admin/cluster/stats/ClusterStatsResponse.java
@@ -1,0 +1,135 @@
+/*
+ * Licensed to ElasticSearch and Shay Banon under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership. ElasticSearch licenses this
+ * file to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+package org.elasticsearch.action.admin.cluster.stats;
+
+import org.elasticsearch.action.support.nodes.NodesOperationResponse;
+import org.elasticsearch.cluster.ClusterName;
+import org.elasticsearch.common.io.stream.StreamInput;
+import org.elasticsearch.common.io.stream.StreamOutput;
+import org.elasticsearch.common.xcontent.ToXContent;
+import org.elasticsearch.common.xcontent.XContentBuilder;
+import org.elasticsearch.common.xcontent.XContentBuilderString;
+import org.elasticsearch.common.xcontent.XContentFactory;
+
+import java.io.IOException;
+import java.util.Iterator;
+import java.util.Map;
+
+/**
+ *
+ */
+public class ClusterStatsResponse extends NodesOperationResponse<ClusterStatsNodeResponse> implements ToXContent {
+
+
+    ClusterStatsNodes nodesStats;
+    ClusterStatsIndices indicesStats;
+    String clusterUUID;
+
+
+    ClusterStatsResponse() {
+    }
+
+    public ClusterStatsResponse(ClusterName clusterName, String clusterUUID, ClusterStatsNodeResponse[] nodes) {
+        super(clusterName, null);
+        this.clusterUUID = clusterUUID;
+        nodesStats = new ClusterStatsNodes(nodes);
+        indicesStats = new ClusterStatsIndices(nodes);
+    }
+
+    public ClusterStatsNodes nodesStats() {
+        return nodesStats;
+    }
+
+    public ClusterStatsIndices indicesStats() {
+        return indicesStats;
+    }
+
+    @Override
+    public ClusterStatsNodeResponse[] getNodes() {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public Map<String, ClusterStatsNodeResponse> getNodesMap() {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public ClusterStatsNodeResponse getAt(int position) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public Iterator<ClusterStatsNodeResponse> iterator() {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public void readFrom(StreamInput in) throws IOException {
+        super.readFrom(in);
+        clusterUUID = in.readString();
+        nodesStats = ClusterStatsNodes.readNodeStats(in);
+        indicesStats = ClusterStatsIndices.readIndicesStats(in);
+    }
+
+    @Override
+    public void writeTo(StreamOutput out) throws IOException {
+        super.writeTo(out);
+        out.writeString(clusterUUID);
+        nodesStats.writeTo(out);
+        indicesStats.writeTo(out);
+    }
+
+    static final class Fields {
+        static final XContentBuilderString NODES = new XContentBuilderString("nodes");
+        static final XContentBuilderString INDICES = new XContentBuilderString("indices");
+        static final XContentBuilderString UUID = new XContentBuilderString("uuid");
+        static final XContentBuilderString CLUSTER_NAME = new XContentBuilderString("cluster_name");
+    }
+
+    @Override
+    public XContentBuilder toXContent(XContentBuilder builder, Params params) throws IOException {
+        builder.field(Fields.CLUSTER_NAME, getClusterName().value());
+        if (params.paramAsBoolean("output_uuid", false)) {
+            builder.field(Fields.UUID, clusterUUID);
+        }
+
+        builder.startObject(Fields.INDICES);
+        indicesStats.toXContent(builder, params);
+        builder.endObject();
+        builder.startObject(Fields.NODES);
+        nodesStats.toXContent(builder, params);
+        builder.endObject();
+        return builder;
+    }
+
+    @Override
+    public String toString() {
+        try {
+            XContentBuilder builder = XContentFactory.jsonBuilder().prettyPrint();
+            builder.startObject();
+            toXContent(builder, EMPTY_PARAMS);
+            builder.endObject();
+            return builder.string();
+        } catch (IOException e) {
+            return "{ \"error\" : \"" + e.getMessage() + "\"}";
+        }
+    }
+}

src/test/java/org/elasticsearch/action/admin/cluster/stats/ClusterStatsTests.java
@@ -1,0 +1,122 @@
+package org.elasticsearch.action.admin.cluster.stats;
+/*
+ * Licensed to ElasticSearch under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership. ElasticSearch licenses this
+ * file to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+
+import org.elasticsearch.common.settings.ImmutableSettings;
+import org.elasticsearch.test.ElasticsearchIntegrationTest;
+import org.elasticsearch.test.ElasticsearchIntegrationTest.ClusterScope;
+import org.hamcrest.Matchers;
+import org.junit.Test;
+
+@ClusterScope(scope = ElasticsearchIntegrationTest.Scope.TEST, numNodes = 0)
+public class ClusterStatsTests extends ElasticsearchIntegrationTest {
+
+    private void assertCounts(ClusterStatsNodes.Counts counts, int total, int masterOnly, int dataOnly, int masterData, int client) {
+        assertThat(counts.total(), Matchers.equalTo(total));
+        assertThat(counts.masterOnly(), Matchers.equalTo(masterOnly));
+        assertThat(counts.dataOnly(), Matchers.equalTo(dataOnly));
+        assertThat(counts.masterData(), Matchers.equalTo(masterData));
+        assertThat(counts.client(), Matchers.equalTo(client));
+    }
+
+    @Test
+    public void testNodeCounts() {
+        cluster().startNode();
+        ClusterStatsResponse response = client().admin().cluster().prepareClusterStats().get();
+        assertCounts(response.nodesStats().counts(), 1, 0, 0, 1, 0);
+
+        cluster().startNode(ImmutableSettings.builder().put("node.data", false));
+        response = client().admin().cluster().prepareClusterStats().get();
+        assertCounts(response.nodesStats().counts(), 2, 1, 0, 1, 0);
+
+        cluster().startNode(ImmutableSettings.builder().put("node.master", false));
+        response = client().admin().cluster().prepareClusterStats().get();
+        assertCounts(response.nodesStats().counts(), 3, 1, 1, 1, 0);
+
+        cluster().startNode(ImmutableSettings.builder().put("node.client", true));
+        response = client().admin().cluster().prepareClusterStats().get();
+        assertCounts(response.nodesStats().counts(), 4, 1, 1, 1, 1);
+    }
+
+
+    private void assertShardStats(ClusterStatsIndices.ShardStats stats, int indices, int total, int primaries, double replicationFactor) {
+        assertThat(stats.indices(), Matchers.equalTo(indices));
+        assertThat(stats.total(), Matchers.equalTo(total));
+        assertThat(stats.primaries(), Matchers.equalTo(primaries));
+        assertThat(stats.replication(), Matchers.equalTo(replicationFactor));
+    }
+
+    @Test
+    public void testIndicesShardStats() {
+        cluster().startNode();
+        prepareCreate("test1").setSettings("number_of_shards", 2, "number_of_replicas", 1).get();
+        ensureYellow();
+        ClusterStatsResponse response = client().admin().cluster().prepareClusterStats().get();
+        assertThat(response.indicesStats.docs().getCount(), Matchers.equalTo(0l));
+        assertThat(response.indicesStats.indexCount(), Matchers.equalTo(1));
+        assertShardStats(response.indicesStats().shards(), 1, 2, 2, 0.0);
+
+        // add another node, replicas should get assigned
+        cluster().startNode();
+        ensureGreen();
+        index("test1", "type", "1", "f", "f");
+        refresh(); // make the doc visible
+        response = client().admin().cluster().prepareClusterStats().get();
+        assertThat(response.indicesStats.docs().getCount(), Matchers.equalTo(1l));
+        assertShardStats(response.indicesStats().shards(), 1, 4, 2, 1.0);
+
+        prepareCreate("test2").setSettings("number_of_shards", 3, "number_of_replicas", 0).get();
+        ensureGreen();
+        response = client().admin().cluster().prepareClusterStats().get();
+        assertThat(response.indicesStats.indexCount(), Matchers.equalTo(2));
+        assertShardStats(response.indicesStats().shards(), 2, 7, 5, 2.0 / 5);
+
+        assertThat(response.indicesStats().shards().avgIndexPrimaryShards(), Matchers.equalTo(2.5));
+        assertThat(response.indicesStats().shards().minIndexPrimaryShards(), Matchers.equalTo(2));
+        assertThat(response.indicesStats().shards().maxIndexPrimaryShards(), Matchers.equalTo(3));
+
+        assertThat(response.indicesStats().shards().avgIndexShards(), Matchers.equalTo(3.5));
+        assertThat(response.indicesStats().shards().minIndexShards(), Matchers.equalTo(3));
+        assertThat(response.indicesStats().shards().maxIndexShards(), Matchers.equalTo(4));
+
+        assertThat(response.indicesStats().shards().avgIndexReplication(), Matchers.equalTo(0.5));
+        assertThat(response.indicesStats().shards().minIndexReplication(), Matchers.equalTo(0.0));
+        assertThat(response.indicesStats().shards().maxIndexReplication(), Matchers.equalTo(1.0));
+
+    }
+
+    @Test
+    public void testValuesSmokeScreen() {
+        cluster().ensureAtMostNumNodes(3);
+        index("test1", "type", "1", "f", "f");
+
+        ClusterStatsResponse response = client().admin().cluster().prepareClusterStats().get();
+        assertThat(response.indicesStats.store().getSizeInBytes(), Matchers.greaterThan(0l));
+
+        assertThat(response.nodesStats.fs().getTotal().bytes(), Matchers.greaterThan(0l));
+        assertThat(response.nodesStats.jvm().versions().size(), Matchers.greaterThan(0));
+        assertThat(response.nodesStats.os().availableProcessors(), Matchers.greaterThan(0));
+        assertThat(response.nodesStats.os().availableMemory().bytes(), Matchers.greaterThan(0l));
+        assertThat(response.nodesStats.os().cpus().size(), Matchers.greaterThan(0));
+        assertThat(response.nodesStats.versions().size(), Matchers.greaterThan(0));
+        assertThat(response.nodesStats.plugins().size(), Matchers.greaterThanOrEqualTo(0));
+
+    }
+}

openeuler-mirror/poissonsearch-oss

src/main/java/org/elasticsearch/action/admin/cluster/stats/ClusterStatsResponse.java
@@ -1,0 +1,135 @@
+/*
+ * Licensed to ElasticSearch and Shay Banon under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership. ElasticSearch licenses this
+ * file to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+package org.elasticsearch.action.admin.cluster.stats;
+
+import org.elasticsearch.action.support.nodes.NodesOperationResponse;
+import org.elasticsearch.cluster.ClusterName;
+import org.elasticsearch.common.io.stream.StreamInput;
+import org.elasticsearch.common.io.stream.StreamOutput;
+import org.elasticsearch.common.xcontent.ToXContent;
+import org.elasticsearch.common.xcontent.XContentBuilder;
+import org.elasticsearch.common.xcontent.XContentBuilderString;
+import org.elasticsearch.common.xcontent.XContentFactory;
+
+import java.io.IOException;
+import java.util.Iterator;
+import java.util.Map;
+
+/**
+ *
+ */
+public class ClusterStatsResponse extends NodesOperationResponse<ClusterStatsNodeResponse> implements ToXContent {
+
+
+    ClusterStatsNodes nodesStats;
+    ClusterStatsIndices indicesStats;
+    String clusterUUID;
+
+
+    ClusterStatsResponse() {
+    }
+
+    public ClusterStatsResponse(ClusterName clusterName, String clusterUUID, ClusterStatsNodeResponse[] nodes) {
+        super(clusterName, null);
+        this.clusterUUID = clusterUUID;
+        nodesStats = new ClusterStatsNodes(nodes);
+        indicesStats = new ClusterStatsIndices(nodes);
+    }
+
+    public ClusterStatsNodes getNodesStats() {
+        return nodesStats;
+    }
+
+    public ClusterStatsIndices getIndicesStats() {
+        return indicesStats;
+    }
+
+    @Override
+    public ClusterStatsNodeResponse[] getNodes() {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public Map<String, ClusterStatsNodeResponse> getNodesMap() {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public ClusterStatsNodeResponse getAt(int position) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public Iterator<ClusterStatsNodeResponse> iterator() {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public void readFrom(StreamInput in) throws IOException {
+        super.readFrom(in);
+        clusterUUID = in.readString();
+        nodesStats = ClusterStatsNodes.readNodeStats(in);
+        indicesStats = ClusterStatsIndices.readIndicesStats(in);
+    }
+
+    @Override
+    public void writeTo(StreamOutput out) throws IOException {
+        super.writeTo(out);
+        out.writeString(clusterUUID);
+        nodesStats.writeTo(out);
+        indicesStats.writeTo(out);
+    }
+
+    static final class Fields {
+        static final XContentBuilderString NODES = new XContentBuilderString("nodes");
+        static final XContentBuilderString INDICES = new XContentBuilderString("indices");
+        static final XContentBuilderString UUID = new XContentBuilderString("uuid");
+        static final XContentBuilderString CLUSTER_NAME = new XContentBuilderString("cluster_name");
+    }
+
+    @Override
+    public XContentBuilder toXContent(XContentBuilder builder, Params params) throws IOException {
+        builder.field(Fields.CLUSTER_NAME, getClusterName().value());
+        if (params.paramAsBoolean("output_uuid", false)) {
+            builder.field(Fields.UUID, clusterUUID);
+        }
+
+        builder.startObject(Fields.INDICES);
+        indicesStats.toXContent(builder, params);
+        builder.endObject();
+        builder.startObject(Fields.NODES);
+        nodesStats.toXContent(builder, params);
+        builder.endObject();
+        return builder;
+    }
+
+    @Override
+    public String toString() {
+        try {
+            XContentBuilder builder = XContentFactory.jsonBuilder().prettyPrint();
+            builder.startObject();
+            toXContent(builder, EMPTY_PARAMS);
+            builder.endObject();
+            return builder.string();
+        } catch (IOException e) {
+            return "{ \"error\" : \"" + e.getMessage() + "\"}";
+        }
+    }
+}

src/test/java/org/elasticsearch/action/admin/cluster/stats/ClusterStatsTests.java
@@ -1,0 +1,130 @@
+package org.elasticsearch.action.admin.cluster.stats;
+/*
+ * Licensed to ElasticSearch under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership. ElasticSearch licenses this
+ * file to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+
+import org.elasticsearch.Version;
+import org.elasticsearch.common.settings.ImmutableSettings;
+import org.elasticsearch.monitor.sigar.SigarService;
+import org.elasticsearch.test.ElasticsearchIntegrationTest;
+import org.elasticsearch.test.ElasticsearchIntegrationTest.ClusterScope;
+import org.hamcrest.Matchers;
+import org.junit.Test;
+
+@ClusterScope(scope = ElasticsearchIntegrationTest.Scope.TEST, numNodes = 0)
+public class ClusterStatsTests extends ElasticsearchIntegrationTest {
+
+    private void assertCounts(ClusterStatsNodes.Counts counts, int total, int masterOnly, int dataOnly, int masterData, int client) {
+        assertThat(counts.getTotal(), Matchers.equalTo(total));
+        assertThat(counts.getMasterOnly(), Matchers.equalTo(masterOnly));
+        assertThat(counts.getDataOnly(), Matchers.equalTo(dataOnly));
+        assertThat(counts.getMasterData(), Matchers.equalTo(masterData));
+        assertThat(counts.getClient(), Matchers.equalTo(client));
+    }
+
+    @Test
+    public void testNodeCounts() {
+        cluster().startNode();
+        ClusterStatsResponse response = client().admin().cluster().prepareClusterStats().get();
+        assertCounts(response.getNodesStats().getCounts(), 1, 0, 0, 1, 0);
+
+        cluster().startNode(ImmutableSettings.builder().put("node.data", false));
+        response = client().admin().cluster().prepareClusterStats().get();
+        assertCounts(response.getNodesStats().getCounts(), 2, 1, 0, 1, 0);
+
+        cluster().startNode(ImmutableSettings.builder().put("node.master", false));
+        response = client().admin().cluster().prepareClusterStats().get();
+        assertCounts(response.getNodesStats().getCounts(), 3, 1, 1, 1, 0);
+
+        cluster().startNode(ImmutableSettings.builder().put("node.client", true));
+        response = client().admin().cluster().prepareClusterStats().get();
+        assertCounts(response.getNodesStats().getCounts(), 4, 1, 1, 1, 1);
+    }
+
+
+    private void assertShardStats(ClusterStatsIndices.ShardStats stats, int indices, int total, int primaries, double replicationFactor) {
+        assertThat(stats.getIndices(), Matchers.equalTo(indices));
+        assertThat(stats.getTotal(), Matchers.equalTo(total));
+        assertThat(stats.getPrimaries(), Matchers.equalTo(primaries));
+        assertThat(stats.getReplication(), Matchers.equalTo(replicationFactor));
+    }
+
+    @Test
+    public void testIndicesShardStats() {
+        cluster().startNode();
+        prepareCreate("test1").setSettings("number_of_shards", 2, "number_of_replicas", 1).get();
+        ensureYellow();
+        ClusterStatsResponse response = client().admin().cluster().prepareClusterStats().get();
+        assertThat(response.indicesStats.getDocs().getCount(), Matchers.equalTo(0l));
+        assertThat(response.indicesStats.getIndexCount(), Matchers.equalTo(1));
+        assertShardStats(response.getIndicesStats().getShards(), 1, 2, 2, 0.0);
+
+        // add another node, replicas should get assigned
+        cluster().startNode();
+        ensureGreen();
+        index("test1", "type", "1", "f", "f");
+        refresh(); // make the doc visible
+        response = client().admin().cluster().prepareClusterStats().get();
+        assertThat(response.indicesStats.getDocs().getCount(), Matchers.equalTo(1l));
+        assertShardStats(response.getIndicesStats().getShards(), 1, 4, 2, 1.0);
+
+        prepareCreate("test2").setSettings("number_of_shards", 3, "number_of_replicas", 0).get();
+        ensureGreen();
+        response = client().admin().cluster().prepareClusterStats().get();
+        assertThat(response.indicesStats.getIndexCount(), Matchers.equalTo(2));
+        assertShardStats(response.getIndicesStats().getShards(), 2, 7, 5, 2.0 / 5);
+
+        assertThat(response.getIndicesStats().getShards().getAvgIndexPrimaryShards(), Matchers.equalTo(2.5));
+        assertThat(response.getIndicesStats().getShards().getMinIndexPrimaryShards(), Matchers.equalTo(2));
+        assertThat(response.getIndicesStats().getShards().getMaxIndexPrimaryShards(), Matchers.equalTo(3));
+
+        assertThat(response.getIndicesStats().getShards().getAvgIndexShards(), Matchers.equalTo(3.5));
+        assertThat(response.getIndicesStats().getShards().getMinIndexShards(), Matchers.equalTo(3));
+        assertThat(response.getIndicesStats().getShards().getMaxIndexShards(), Matchers.equalTo(4));
+
+        assertThat(response.getIndicesStats().getShards().getAvgIndexReplication(), Matchers.equalTo(0.5));
+        assertThat(response.getIndicesStats().getShards().getMinIndexReplication(), Matchers.equalTo(0.0));
+        assertThat(response.getIndicesStats().getShards().getMaxIndexReplication(), Matchers.equalTo(1.0));
+
+    }
+
+    @Test
+    public void testValuesSmokeScreen() {
+        cluster().ensureAtMostNumNodes(5);
+        cluster().ensureAtLeastNumNodes(1);
+        SigarService sigarService = cluster().getInstance(SigarService.class);
+        index("test1", "type", "1", "f", "f");
+
+        ClusterStatsResponse response = client().admin().cluster().prepareClusterStats().get();
+        assertThat(response.indicesStats.getStore().getSizeInBytes(), Matchers.greaterThan(0l));
+
+        assertThat(response.nodesStats.getFs().getTotal().bytes(), Matchers.greaterThan(0l));
+        assertThat(response.nodesStats.getJvm().getVersions().size(), Matchers.greaterThan(0));
+        if (sigarService.sigarAvailable()) {
+            // We only get those if we have sigar
+            assertThat(response.nodesStats.getOs().getAvailableProcessors(), Matchers.greaterThan(0));
+            assertThat(response.nodesStats.getOs().getAvailableMemory().bytes(), Matchers.greaterThan(0l));
+            assertThat(response.nodesStats.getOs().getCpus().size(), Matchers.greaterThan(0));
+        }
+        assertThat(response.nodesStats.getVersions().size(), Matchers.greaterThan(0));
+        assertThat(response.nodesStats.getVersions().contains(Version.CURRENT), Matchers.equalTo(true));
+        assertThat(response.nodesStats.getPlugins().size(), Matchers.greaterThanOrEqualTo(0));
+
+    }
+}