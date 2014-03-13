elastic/elasticsearch

Make aggregations CacheRecycler-free.
Aggregations were still using CacheRecycler on the reduce phase. They are now
using page-based recycling for both the aggregation phase and the reduce phase.

Close #4929

src/main/java/org/elasticsearch/search/aggregations/bucket/terms/DoubleTerms.java
@@ -18,12 +18,11 @@
  */
 package org.elasticsearch.search.aggregations.bucket.terms;
 
-import com.carrotsearch.hppc.DoubleObjectOpenHashMap;
 import org.elasticsearch.common.io.stream.StreamInput;
 import org.elasticsearch.common.io.stream.StreamOutput;
-import org.elasticsearch.common.recycler.Recycler;
 import org.elasticsearch.common.text.StringText;
 import org.elasticsearch.common.text.Text;
+import org.elasticsearch.common.util.DoubleObjectHashMap;
 import org.elasticsearch.common.xcontent.XContentBuilder;
 import org.elasticsearch.search.aggregations.AggregationStreams;
 import org.elasticsearch.search.aggregations.InternalAggregation;
@@ -112,12 +111,12 @@ public class DoubleTerms extends InternalTerms {
         List<InternalAggregation> aggregations = reduceContext.aggregations();
         if (aggregations.size() == 1) {
             InternalTerms terms = (InternalTerms) aggregations.get(0);
-            terms.trimExcessEntries(reduceContext.cacheRecycler());
+            terms.trimExcessEntries(reduceContext.bigArrays());
             return terms;
         }
         InternalTerms reduced = null;
 
-        Recycler.V<DoubleObjectOpenHashMap<List<Bucket>>> buckets = null;
+        DoubleObjectHashMap<List<Bucket>> buckets = null;
         for (InternalAggregation aggregation : aggregations) {
             InternalTerms terms = (InternalTerms) aggregation;
             if (terms instanceof UnmappedTerms) {
@@ -127,13 +126,13 @@ public class DoubleTerms extends InternalTerms {
                 reduced = terms;
             }
             if (buckets == null) {
-                buckets = reduceContext.cacheRecycler().doubleObjectMap(terms.buckets.size());
+                buckets = new DoubleObjectHashMap<List<Bucket>>(terms.buckets.size(), reduceContext.bigArrays());
             }
             for (Terms.Bucket bucket : terms.buckets) {
-                List<Bucket> existingBuckets = buckets.v().get(((Bucket) bucket).term);
+                List<Bucket> existingBuckets = buckets.get(((Bucket) bucket).term);
                 if (existingBuckets == null) {
                     existingBuckets = new ArrayList<Bucket>(aggregations.size());
-                    buckets.v().put(((Bucket) bucket).term, existingBuckets);
+                    buckets.put(((Bucket) bucket).term, existingBuckets);
                 }
                 existingBuckets.add((Bucket) bucket);
             }
@@ -145,17 +144,13 @@ public class DoubleTerms extends InternalTerms {
         }
 
         // TODO: would it be better to sort the backing array buffer of hppc map directly instead of using a PQ?
-        final int size = Math.min(requiredSize, buckets.v().size());
+        final int size = (int) Math.min(requiredSize, buckets.size());
         BucketPriorityQueue ordered = new BucketPriorityQueue(size, order.comparator(null));
-        boolean[] states = buckets.v().allocated;
-        Object[] internalBuckets = buckets.v().values;
-        for (int i = 0; i < states.length; i++) {
-            if (states[i]) {
-                List<DoubleTerms.Bucket> sameTermBuckets = (List<DoubleTerms.Bucket>) internalBuckets[i];
-                final InternalTerms.Bucket b = sameTermBuckets.get(0).reduce(sameTermBuckets, reduceContext.cacheRecycler());
-                if (b.getDocCount() >= minDocCount) {
-                    ordered.insertWithOverflow(b);
-                }
+        for (DoubleObjectHashMap.Cursor<List<DoubleTerms.Bucket>> cursor : buckets) {
+            List<DoubleTerms.Bucket> sameTermBuckets = cursor.value;
+            final InternalTerms.Bucket b = sameTermBuckets.get(0).reduce(sameTermBuckets, reduceContext.bigArrays());
+            if (b.getDocCount() >= minDocCount) {
+                ordered.insertWithOverflow(b);
             }
         }
         buckets.release();

src/main/java/org/elasticsearch/search/aggregations/bucket/terms/LongTerms.java
@@ -18,13 +18,12 @@
  */
 package org.elasticsearch.search.aggregations.bucket.terms;
 
-import com.carrotsearch.hppc.LongObjectOpenHashMap;
 import com.google.common.primitives.Longs;
 import org.elasticsearch.common.io.stream.StreamInput;
 import org.elasticsearch.common.io.stream.StreamOutput;
-import org.elasticsearch.common.recycler.Recycler;
 import org.elasticsearch.common.text.StringText;
 import org.elasticsearch.common.text.Text;
+import org.elasticsearch.common.util.LongObjectHashMap;
 import org.elasticsearch.common.xcontent.XContentBuilder;
 import org.elasticsearch.search.aggregations.AggregationStreams;
 import org.elasticsearch.search.aggregations.InternalAggregation;
@@ -109,12 +108,12 @@ public class LongTerms extends InternalTerms {
         List<InternalAggregation> aggregations = reduceContext.aggregations();
         if (aggregations.size() == 1) {
             InternalTerms terms = (InternalTerms) aggregations.get(0);
-            terms.trimExcessEntries(reduceContext.cacheRecycler());
+            terms.trimExcessEntries(reduceContext.bigArrays());
             return terms;
         }
         InternalTerms reduced = null;
 
-        Recycler.V<LongObjectOpenHashMap<List<Bucket>>> buckets = null;
+        LongObjectHashMap<List<Bucket>> buckets = null;
         for (InternalAggregation aggregation : aggregations) {
             InternalTerms terms = (InternalTerms) aggregation;
             if (terms instanceof UnmappedTerms) {
@@ -124,13 +123,13 @@ public class LongTerms extends InternalTerms {
                 reduced = terms;
             }
             if (buckets == null) {
-                buckets = reduceContext.cacheRecycler().longObjectMap(terms.buckets.size());
+                buckets = new LongObjectHashMap<List<Bucket>>(terms.buckets.size(), reduceContext.bigArrays());
             }
             for (Terms.Bucket bucket : terms.buckets) {
-                List<Bucket> existingBuckets = buckets.v().get(((Bucket) bucket).term);
+                List<Bucket> existingBuckets = buckets.get(((Bucket) bucket).term);
                 if (existingBuckets == null) {
                     existingBuckets = new ArrayList<Bucket>(aggregations.size());
-                    buckets.v().put(((Bucket) bucket).term, existingBuckets);
+                    buckets.put(((Bucket) bucket).term, existingBuckets);
                 }
                 existingBuckets.add((Bucket) bucket);
             }
@@ -142,17 +141,13 @@ public class LongTerms extends InternalTerms {
         }
 
         // TODO: would it be better to sort the backing array buffer of the hppc map directly instead of using a PQ?
-        final int size = Math.min(requiredSize, buckets.v().size());
+        final int size = (int) Math.min(requiredSize, buckets.size());
         BucketPriorityQueue ordered = new BucketPriorityQueue(size, order.comparator(null));
-        Object[] internalBuckets = buckets.v().values;
-        boolean[] states = buckets.v().allocated;
-        for (int i = 0; i < states.length; i++) {
-            if (states[i]) {
-                List<LongTerms.Bucket> sameTermBuckets = (List<LongTerms.Bucket>) internalBuckets[i];
-                final InternalTerms.Bucket b = sameTermBuckets.get(0).reduce(sameTermBuckets, reduceContext.cacheRecycler());
-                if (b.getDocCount() >= minDocCount) {
-                    ordered.insertWithOverflow(b);
-                }
+        for (LongObjectHashMap.Cursor<List<LongTerms.Bucket>> cursor : buckets) {
+            List<LongTerms.Bucket> sameTermBuckets = cursor.value;
+            final InternalTerms.Bucket b = sameTermBuckets.get(0).reduce(sameTermBuckets, reduceContext.bigArrays());
+            if (b.getDocCount() >= minDocCount) {
+                ordered.insertWithOverflow(b);
             }
         }
         buckets.release();

src/test/java/org/elasticsearch/common/util/DoubleObjectHashMapTests.java
@@ -1,0 +1,59 @@
+/*
+ * Licensed to Elasticsearch under one or more contributor
+ * license agreements. See the NOTICE file distributed with
+ * this work for additional information regarding copyright
+ * ownership. Elasticsearch licenses this file to you under
+ * the Apache License, Version 2.0 (the "License"); you may
+ * not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+package org.elasticsearch.common.util;
+
+import com.carrotsearch.hppc.DoubleObjectOpenHashMap;
+import org.elasticsearch.test.ElasticsearchTestCase;
+import org.junit.Test;
+
+public class DoubleObjectHashMapTests extends ElasticsearchTestCase {
+
+    @Test
+    public void duel() {
+        final DoubleObjectOpenHashMap<Object> map1 = new DoubleObjectOpenHashMap<Object>();
+        final DoubleObjectHashMap<Object> map2 = new DoubleObjectHashMap<Object>(randomInt(42), 0.6f + randomFloat() * 0.39f, BigArraysTests.randombigArrays());
+        final int maxKey = randomIntBetween(1, 10000);
+        final int iters = atLeast(10000);
+        for (int i = 0; i < iters; ++i) {
+            final boolean put = randomBoolean();
+            final int iters2 = randomIntBetween(1, 100);
+            for (int j = 0; j < iters2; ++j) {
+                final double key = randomInt(maxKey);
+                if (put) {
+                    final Object value = new Object();
+                    assertSame(map1.put(key, value), map2.put(key, value));
+                } else {
+                    assertSame(map1.remove(key), map2.remove(key));
+                }
+                assertEquals(map1.size(), map2.size());
+            }
+        }
+        for (int i = 0; i <= maxKey; ++i) {
+            assertSame(map1.get(i), map2.get(i));
+        }
+        final DoubleObjectOpenHashMap<Object> copy = new DoubleObjectOpenHashMap<Object>();
+        for (DoubleObjectHashMap.Cursor<Object> cursor : map2) {
+            copy.put(cursor.key, cursor.value);
+        }
+        map2.release();
+        assertEquals(map1, copy);
+    }
+
+}

src/test/java/org/elasticsearch/common/util/LongObjectHashMapTests.java
@@ -1,0 +1,59 @@
+/*
+ * Licensed to Elasticsearch under one or more contributor
+ * license agreements. See the NOTICE file distributed with
+ * this work for additional information regarding copyright
+ * ownership. Elasticsearch licenses this file to you under
+ * the Apache License, Version 2.0 (the "License"); you may
+ * not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+package org.elasticsearch.common.util;
+
+import com.carrotsearch.hppc.LongObjectOpenHashMap;
+import org.elasticsearch.test.ElasticsearchTestCase;
+import org.junit.Test;
+
+public class LongObjectHashMapTests extends ElasticsearchTestCase {
+
+    @Test
+    public void duel() {
+        final LongObjectOpenHashMap<Object> map1 = new LongObjectOpenHashMap<Object>();
+        final LongObjectHashMap<Object> map2 = new LongObjectHashMap<Object>(randomInt(42), 0.6f + randomFloat() * 0.39f, BigArraysTests.randombigArrays());
+        final int maxKey = randomIntBetween(1, 10000);
+        final int iters = atLeast(10000);
+        for (int i = 0; i < iters; ++i) {
+            final boolean put = randomBoolean();
+            final int iters2 = randomIntBetween(1, 100);
+            for (int j = 0; j < iters2; ++j) {
+                final long key = randomInt(maxKey);
+                if (put) {
+                    final Object value = new Object();
+                    assertSame(map1.put(key, value), map2.put(key, value));
+                } else {
+                    assertSame(map1.remove(key), map2.remove(key));
+                }
+                assertEquals(map1.size(), map2.size());
+            }
+        }
+        for (int i = 0; i <= maxKey; ++i) {
+            assertSame(map1.get(i), map2.get(i));
+        }
+        final LongObjectOpenHashMap<Object> copy = new LongObjectOpenHashMap<Object>();
+        for (LongObjectHashMap.Cursor<Object> cursor : map2) {
+            copy.put(cursor.key, cursor.value);
+        }
+        map2.release();
+        assertEquals(map1, copy);
+    }
+
+}

openeuler-mirror/poissonsearch-oss

src/main/java/org/elasticsearch/search/aggregations/bucket/terms/DoubleTerms.java
@@ -18,12 +18,11 @@
  */
 package org.elasticsearch.search.aggregations.bucket.terms;
 
-import com.carrotsearch.hppc.DoubleObjectOpenHashMap;
 import org.elasticsearch.common.io.stream.StreamInput;
 import org.elasticsearch.common.io.stream.StreamOutput;
-import org.elasticsearch.common.recycler.Recycler;
 import org.elasticsearch.common.text.StringText;
 import org.elasticsearch.common.text.Text;
+import org.elasticsearch.common.util.DoubleObjectPagedHashMap;
 import org.elasticsearch.common.xcontent.XContentBuilder;
 import org.elasticsearch.search.aggregations.AggregationStreams;
 import org.elasticsearch.search.aggregations.InternalAggregation;
@@ -112,12 +111,12 @@ public class DoubleTerms extends InternalTerms {
         List<InternalAggregation> aggregations = reduceContext.aggregations();
         if (aggregations.size() == 1) {
             InternalTerms terms = (InternalTerms) aggregations.get(0);
-            terms.trimExcessEntries(reduceContext.cacheRecycler());
+            terms.trimExcessEntries(reduceContext.bigArrays());
             return terms;
         }
         InternalTerms reduced = null;
 
-        Recycler.V<DoubleObjectOpenHashMap<List<Bucket>>> buckets = null;
+        DoubleObjectPagedHashMap<List<Bucket>> buckets = null;
         for (InternalAggregation aggregation : aggregations) {
             InternalTerms terms = (InternalTerms) aggregation;
             if (terms instanceof UnmappedTerms) {
@@ -127,13 +126,13 @@ public class DoubleTerms extends InternalTerms {
                 reduced = terms;
             }
             if (buckets == null) {
-                buckets = reduceContext.cacheRecycler().doubleObjectMap(terms.buckets.size());
+                buckets = new DoubleObjectPagedHashMap<List<Bucket>>(terms.buckets.size(), reduceContext.bigArrays());
             }
             for (Terms.Bucket bucket : terms.buckets) {
-                List<Bucket> existingBuckets = buckets.v().get(((Bucket) bucket).term);
+                List<Bucket> existingBuckets = buckets.get(((Bucket) bucket).term);
                 if (existingBuckets == null) {
                     existingBuckets = new ArrayList<Bucket>(aggregations.size());
-                    buckets.v().put(((Bucket) bucket).term, existingBuckets);
+                    buckets.put(((Bucket) bucket).term, existingBuckets);
                 }
                 existingBuckets.add((Bucket) bucket);
             }
@@ -145,17 +144,13 @@ public class DoubleTerms extends InternalTerms {
         }
 
         // TODO: would it be better to sort the backing array buffer of hppc map directly instead of using a PQ?
-        final int size = Math.min(requiredSize, buckets.v().size());
+        final int size = (int) Math.min(requiredSize, buckets.size());
         BucketPriorityQueue ordered = new BucketPriorityQueue(size, order.comparator(null));
-        boolean[] states = buckets.v().allocated;
-        Object[] internalBuckets = buckets.v().values;
-        for (int i = 0; i < states.length; i++) {
-            if (states[i]) {
-                List<DoubleTerms.Bucket> sameTermBuckets = (List<DoubleTerms.Bucket>) internalBuckets[i];
-                final InternalTerms.Bucket b = sameTermBuckets.get(0).reduce(sameTermBuckets, reduceContext.cacheRecycler());
-                if (b.getDocCount() >= minDocCount) {
-                    ordered.insertWithOverflow(b);
-                }
+        for (DoubleObjectPagedHashMap.Cursor<List<DoubleTerms.Bucket>> cursor : buckets) {
+            List<DoubleTerms.Bucket> sameTermBuckets = cursor.value;
+            final InternalTerms.Bucket b = sameTermBuckets.get(0).reduce(sameTermBuckets, reduceContext.bigArrays());
+            if (b.getDocCount() >= minDocCount) {
+                ordered.insertWithOverflow(b);
             }
         }
         buckets.release();

src/main/java/org/elasticsearch/search/aggregations/bucket/terms/LongTerms.java
@@ -18,13 +18,12 @@
  */
 package org.elasticsearch.search.aggregations.bucket.terms;
 
-import com.carrotsearch.hppc.LongObjectOpenHashMap;
 import com.google.common.primitives.Longs;
 import org.elasticsearch.common.io.stream.StreamInput;
 import org.elasticsearch.common.io.stream.StreamOutput;
-import org.elasticsearch.common.recycler.Recycler;
 import org.elasticsearch.common.text.StringText;
 import org.elasticsearch.common.text.Text;
+import org.elasticsearch.common.util.LongObjectPagedHashMap;
 import org.elasticsearch.common.xcontent.XContentBuilder;
 import org.elasticsearch.search.aggregations.AggregationStreams;
 import org.elasticsearch.search.aggregations.InternalAggregation;
@@ -109,12 +108,12 @@ public class LongTerms extends InternalTerms {
         List<InternalAggregation> aggregations = reduceContext.aggregations();
         if (aggregations.size() == 1) {
             InternalTerms terms = (InternalTerms) aggregations.get(0);
-            terms.trimExcessEntries(reduceContext.cacheRecycler());
+            terms.trimExcessEntries(reduceContext.bigArrays());
             return terms;
         }
         InternalTerms reduced = null;
 
-        Recycler.V<LongObjectOpenHashMap<List<Bucket>>> buckets = null;
+        LongObjectPagedHashMap<List<Bucket>> buckets = null;
         for (InternalAggregation aggregation : aggregations) {
             InternalTerms terms = (InternalTerms) aggregation;
             if (terms instanceof UnmappedTerms) {
@@ -124,13 +123,13 @@ public class LongTerms extends InternalTerms {
                 reduced = terms;
             }
             if (buckets == null) {
-                buckets = reduceContext.cacheRecycler().longObjectMap(terms.buckets.size());
+                buckets = new LongObjectPagedHashMap<List<Bucket>>(terms.buckets.size(), reduceContext.bigArrays());
             }
             for (Terms.Bucket bucket : terms.buckets) {
-                List<Bucket> existingBuckets = buckets.v().get(((Bucket) bucket).term);
+                List<Bucket> existingBuckets = buckets.get(((Bucket) bucket).term);
                 if (existingBuckets == null) {
                     existingBuckets = new ArrayList<Bucket>(aggregations.size());
-                    buckets.v().put(((Bucket) bucket).term, existingBuckets);
+                    buckets.put(((Bucket) bucket).term, existingBuckets);
                 }
                 existingBuckets.add((Bucket) bucket);
             }
@@ -142,17 +141,13 @@ public class LongTerms extends InternalTerms {
         }
 
         // TODO: would it be better to sort the backing array buffer of the hppc map directly instead of using a PQ?
-        final int size = Math.min(requiredSize, buckets.v().size());
+        final int size = (int) Math.min(requiredSize, buckets.size());
         BucketPriorityQueue ordered = new BucketPriorityQueue(size, order.comparator(null));
-        Object[] internalBuckets = buckets.v().values;
-        boolean[] states = buckets.v().allocated;
-        for (int i = 0; i < states.length; i++) {
-            if (states[i]) {
-                List<LongTerms.Bucket> sameTermBuckets = (List<LongTerms.Bucket>) internalBuckets[i];
-                final InternalTerms.Bucket b = sameTermBuckets.get(0).reduce(sameTermBuckets, reduceContext.cacheRecycler());
-                if (b.getDocCount() >= minDocCount) {
-                    ordered.insertWithOverflow(b);
-                }
+        for (LongObjectPagedHashMap.Cursor<List<LongTerms.Bucket>> cursor : buckets) {
+            List<LongTerms.Bucket> sameTermBuckets = cursor.value;
+            final InternalTerms.Bucket b = sameTermBuckets.get(0).reduce(sameTermBuckets, reduceContext.bigArrays());
+            if (b.getDocCount() >= minDocCount) {
+                ordered.insertWithOverflow(b);
             }
         }
         buckets.release();

src/test/java/org/elasticsearch/common/util/DoubleObjectHashMapTests.java
@@ -1,0 +1,59 @@
+/*
+ * Licensed to Elasticsearch under one or more contributor
+ * license agreements. See the NOTICE file distributed with
+ * this work for additional information regarding copyright
+ * ownership. Elasticsearch licenses this file to you under
+ * the Apache License, Version 2.0 (the "License"); you may
+ * not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+package org.elasticsearch.common.util;
+
+import com.carrotsearch.hppc.DoubleObjectOpenHashMap;
+import org.elasticsearch.test.ElasticsearchTestCase;
+import org.junit.Test;
+
+public class DoubleObjectHashMapTests extends ElasticsearchTestCase {
+
+    @Test
+    public void duel() {
+        final DoubleObjectOpenHashMap<Object> map1 = new DoubleObjectOpenHashMap<Object>();
+        final DoubleObjectPagedHashMap<Object> map2 = new DoubleObjectPagedHashMap<Object>(randomInt(42), 0.6f + randomFloat() * 0.39f, BigArraysTests.randombigArrays());
+        final int maxKey = randomIntBetween(1, 10000);
+        final int iters = atLeast(10000);
+        for (int i = 0; i < iters; ++i) {
+            final boolean put = randomBoolean();
+            final int iters2 = randomIntBetween(1, 100);
+            for (int j = 0; j < iters2; ++j) {
+                final double key = randomInt(maxKey);
+                if (put) {
+                    final Object value = new Object();
+                    assertSame(map1.put(key, value), map2.put(key, value));
+                } else {
+                    assertSame(map1.remove(key), map2.remove(key));
+                }
+                assertEquals(map1.size(), map2.size());
+            }
+        }
+        for (int i = 0; i <= maxKey; ++i) {
+            assertSame(map1.get(i), map2.get(i));
+        }
+        final DoubleObjectOpenHashMap<Object> copy = new DoubleObjectOpenHashMap<Object>();
+        for (DoubleObjectPagedHashMap.Cursor<Object> cursor : map2) {
+            copy.put(cursor.key, cursor.value);
+        }
+        map2.release();
+        assertEquals(map1, copy);
+    }
+
+}

src/test/java/org/elasticsearch/common/util/LongObjectHashMapTests.java
@@ -1,0 +1,59 @@
+/*
+ * Licensed to Elasticsearch under one or more contributor
+ * license agreements. See the NOTICE file distributed with
+ * this work for additional information regarding copyright
+ * ownership. Elasticsearch licenses this file to you under
+ * the Apache License, Version 2.0 (the "License"); you may
+ * not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+package org.elasticsearch.common.util;
+
+import com.carrotsearch.hppc.LongObjectOpenHashMap;
+import org.elasticsearch.test.ElasticsearchTestCase;
+import org.junit.Test;
+
+public class LongObjectHashMapTests extends ElasticsearchTestCase {
+
+    @Test
+    public void duel() {
+        final LongObjectOpenHashMap<Object> map1 = new LongObjectOpenHashMap<Object>();
+        final LongObjectPagedHashMap<Object> map2 = new LongObjectPagedHashMap<Object>(randomInt(42), 0.6f + randomFloat() * 0.39f, BigArraysTests.randombigArrays());
+        final int maxKey = randomIntBetween(1, 10000);
+        final int iters = atLeast(10000);
+        for (int i = 0; i < iters; ++i) {
+            final boolean put = randomBoolean();
+            final int iters2 = randomIntBetween(1, 100);
+            for (int j = 0; j < iters2; ++j) {
+                final long key = randomInt(maxKey);
+                if (put) {
+                    final Object value = new Object();
+                    assertSame(map1.put(key, value), map2.put(key, value));
+                } else {
+                    assertSame(map1.remove(key), map2.remove(key));
+                }
+                assertEquals(map1.size(), map2.size());
+            }
+        }
+        for (int i = 0; i <= maxKey; ++i) {
+            assertSame(map1.get(i), map2.get(i));
+        }
+        final LongObjectOpenHashMap<Object> copy = new LongObjectOpenHashMap<Object>();
+        for (LongObjectPagedHashMap.Cursor<Object> cursor : map2) {
+            copy.put(cursor.key, cursor.value);
+        }
+        map2.release();
+        assertEquals(map1, copy);
+    }
+
+}