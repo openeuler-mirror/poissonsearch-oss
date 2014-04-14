elastic/elasticsearch

Improved SearchContext.addReleasable.
For resources that have their life time effectively defined by the search
context they are attached to, it is convenient to use the search context to
schedule the release of such resources.

This commit changes aggregations to use this mechanism and also introduces
a `Lifetime` object that can be used to define how long the object should
live:
 - COLLECTION: if the object only needs to live during collection time and is
   what SearchContext.addReleasable would have chosen before this change
   (used for p/c queries),
 - SEARCH_PHASE for resources that only need to live during the current search
   phase (DFS, QUERY or FETCH),
 - SEARCH_CONTEXT for resources that need to live until the context is
   destroyed.

Aggregators are currently registed with SEARCH_CONTEXT. The reason is that when
using the DFS_QUERY_THEN_FETCH search type, they are allocated during the DFS
phase but only used during the QUERY phase. However we should fix it in order
to only allocate them during the QUERY phase and use SEARCH_PHASE as a life
time.

Close #5703

src/main/java/org/elasticsearch/search/aggregations/AggregatorFactories.java
@@ -18,8 +18,6 @@
  */
 package org.elasticsearch.search.aggregations;
 
-import com.google.common.collect.Iterables;
-import com.google.common.collect.UnmodifiableIterator;
 import org.apache.lucene.index.AtomicReaderContext;
 import org.elasticsearch.common.lease.Releasables;
 import org.elasticsearch.common.util.ObjectArray;
@@ -28,8 +26,6 @@ import org.elasticsearch.search.aggregations.support.AggregationContext;
 
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Iterator;
 import java.util.List;
 
 /**
@@ -82,9 +78,6 @@ public class AggregatorFactories {
                     long arraySize = estimatedBucketsCount > 0 ?  estimatedBucketsCount : 1;
                     aggregators = bigArrays.newObjectArray(arraySize);
                     aggregators.set(0, first);
-                    for (long i = 1; i < arraySize; ++i) {
-                        aggregators.set(i, createAndRegisterContextAware(parent.context(), factory, parent, estimatedBucketsCount));
-                    }
                 }
 
                 @Override
@@ -135,29 +128,7 @@ public class AggregatorFactories {
 
                 @Override
                 public void doRelease() {
-                    final Iterable<Aggregator> aggregatorsIter = new Iterable<Aggregator>() {
-
-                        @Override
-                        public Iterator<Aggregator> iterator() {
-                            return new UnmodifiableIterator<Aggregator>() {
-
-                                long i = 0;
-
-                                @Override
-                                public boolean hasNext() {
-                                    return i < aggregators.size();
-                                }
-
-                                @Override
-                                public Aggregator next() {
-                                    return aggregators.get(i++);
-                                }
-
-                            };
-                        }
-
-                    };
-                    Releasables.release(Iterables.concat(aggregatorsIter, Collections.singleton(aggregators)));
+                    Releasables.release(aggregators);
                 }
             };
         }

src/main/java/org/elasticsearch/search/internal/DefaultSearchContext.java
@@ -30,7 +30,6 @@ import org.elasticsearch.action.search.SearchType;
 import org.elasticsearch.cache.recycler.CacheRecycler;
 import org.elasticsearch.cache.recycler.PageCacheRecycler;
 import org.elasticsearch.common.Nullable;
-import org.elasticsearch.common.lease.Releasable;
 import org.elasticsearch.common.lease.Releasables;
 import org.elasticsearch.common.lucene.search.AndFilter;
 import org.elasticsearch.common.lucene.search.Queries;
@@ -177,8 +176,6 @@ public class DefaultSearchContext extends SearchContext {
 
     private volatile long lastAccessTime = -1;
 
-    private List<Releasable> clearables = null;
-
     private volatile boolean useSlowScroll;
 
     public DefaultSearchContext(long id, ShardSearchRequest request, SearchShardTarget shardTarget,
@@ -207,19 +204,12 @@ public class DefaultSearchContext extends SearchContext {
     }
 
     @Override
-    public boolean release() throws ElasticsearchException {
+    public void doRelease() throws ElasticsearchException {
         if (scanContext != null) {
             scanContext.clear();
         }
         // clear and scope phase we  have
-        searcher.release();
-        engineSearcher.release();
-        return true;
-    }
-
-    public boolean clearAndRelease() {
-        clearReleasables();
-        return release();
+        Releasables.release(searcher, engineSearcher);
     }
 
     /**
@@ -676,25 +666,6 @@ public class DefaultSearchContext extends SearchContext {
 
     public FetchSearchResult fetchResult() {
         return fetchResult;
-    }
-
-    @Override
-    public void addReleasable(Releasable releasable) {
-        if (clearables == null) {
-            clearables = new ArrayList<>();
-        }
-        clearables.add(releasable);
-    }
-
-    @Override
-    public void clearReleasables() {
-        if (clearables != null) {
-            try {
-                Releasables.release(clearables);
-            } finally {
-                clearables.clear();
-            }
-        }
     }
 
     public ScanContext scanContext() {

openeuler-mirror/poissonsearch-oss

src/main/java/org/elasticsearch/search/aggregations/AggregatorFactories.java
@@ -18,8 +18,6 @@
  */
 package org.elasticsearch.search.aggregations;
 
-import com.google.common.collect.Iterables;
-import com.google.common.collect.UnmodifiableIterator;
 import org.apache.lucene.index.AtomicReaderContext;
 import org.elasticsearch.common.lease.Releasables;
 import org.elasticsearch.common.util.ObjectArray;
@@ -28,8 +26,6 @@ import org.elasticsearch.search.aggregations.support.AggregationContext;
 
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Iterator;
 import java.util.List;
 
 /**
@@ -82,9 +78,6 @@ public class AggregatorFactories {
                     long arraySize = estimatedBucketsCount > 0 ?  estimatedBucketsCount : 1;
                     aggregators = bigArrays.newObjectArray(arraySize);
                     aggregators.set(0, first);
-                    for (long i = 1; i < arraySize; ++i) {
-                        aggregators.set(i, createAndRegisterContextAware(parent.context(), factory, parent, estimatedBucketsCount));
-                    }
                 }
 
                 @Override
@@ -135,29 +128,7 @@ public class AggregatorFactories {
 
                 @Override
                 public void doClose() {
-                    final Iterable<Aggregator> aggregatorsIter = new Iterable<Aggregator>() {
-
-                        @Override
-                        public Iterator<Aggregator> iterator() {
-                            return new UnmodifiableIterator<Aggregator>() {
-
-                                long i = 0;
-
-                                @Override
-                                public boolean hasNext() {
-                                    return i < aggregators.size();
-                                }
-
-                                @Override
-                                public Aggregator next() {
-                                    return aggregators.get(i++);
-                                }
-
-                            };
-                        }
-
-                    };
-                    Releasables.close(Iterables.concat(aggregatorsIter, Collections.singleton(aggregators)));
+                    Releasables.close(aggregators);
                 }
             };
         }

src/main/java/org/elasticsearch/search/internal/DefaultSearchContext.java
@@ -30,7 +30,6 @@ import org.elasticsearch.action.search.SearchType;
 import org.elasticsearch.cache.recycler.CacheRecycler;
 import org.elasticsearch.cache.recycler.PageCacheRecycler;
 import org.elasticsearch.common.Nullable;
-import org.elasticsearch.common.lease.Releasable;
 import org.elasticsearch.common.lease.Releasables;
 import org.elasticsearch.common.lucene.search.AndFilter;
 import org.elasticsearch.common.lucene.search.Queries;
@@ -177,8 +176,6 @@ public class DefaultSearchContext extends SearchContext {
 
     private volatile long lastAccessTime = -1;
 
-    private List<Releasable> clearables = null;
-
     private volatile boolean useSlowScroll;
 
     public DefaultSearchContext(long id, ShardSearchRequest request, SearchShardTarget shardTarget,
@@ -207,18 +204,12 @@ public class DefaultSearchContext extends SearchContext {
     }
 
     @Override
-    public void close() throws ElasticsearchException {
+    public void doClose() throws ElasticsearchException {
         if (scanContext != null) {
             scanContext.clear();
         }
         // clear and scope phase we  have
-        searcher.release();
-        engineSearcher.close();
-    }
-
-    public void clearAndRelease() {
-        clearReleasables();
-        close();
+        Releasables.close(searcher, engineSearcher);
     }
 
     /**
@@ -675,25 +666,6 @@ public class DefaultSearchContext extends SearchContext {
 
     public FetchSearchResult fetchResult() {
         return fetchResult;
-    }
-
-    @Override
-    public void addReleasable(Releasable releasable) {
-        if (clearables == null) {
-            clearables = new ArrayList<>();
-        }
-        clearables.add(releasable);
-    }
-
-    @Override
-    public void clearReleasables() {
-        if (clearables != null) {
-            try {
-                Releasables.close(clearables);
-            } finally {
-                clearables.clear();
-            }
-        }
     }
 
     public ScanContext scanContext() {