elastic/elasticsearch

Don't use the fixed bitset filter cache for child nested level filters, but the regular filter cache instead.
Random access based bitsets are not required for the child level nested level filters.

Closes #8810

src/main/java/org/elasticsearch/index/fielddata/IndexFieldData.java
@@ -129,9 +129,11 @@ public interface IndexFieldData<FD extends AtomicFieldData> extends IndexCompone
          * parent + 1, or 0 if there is no previous parent, and R (excluded).
          */
         public static class Nested {
-            private final FixedBitSetFilter rootFilter, innerFilter;
 
-            public Nested(FixedBitSetFilter rootFilter, FixedBitSetFilter innerFilter) {
+            private final FixedBitSetFilter rootFilter;
+            private final Filter innerFilter;
+
+            public Nested(FixedBitSetFilter rootFilter, Filter innerFilter) {
                 this.rootFilter = rootFilter;
                 this.innerFilter = innerFilter;
             }
@@ -146,7 +148,7 @@ public interface IndexFieldData<FD extends AtomicFieldData> extends IndexCompone
             /**
              * Get a {@link FixedBitSet} that matches the inner documents.
              */
-            public FixedBitSet innerDocs(AtomicReaderContext ctx) throws IOException {
+            public DocIdSet innerDocs(AtomicReaderContext ctx) throws IOException {
                 return innerFilter.getDocIdSet(ctx, null);
             }
         }

openeuler-mirror/poissonsearch-oss

src/main/java/org/elasticsearch/index/fielddata/IndexFieldData.java
@@ -129,9 +129,11 @@ public interface IndexFieldData<FD extends AtomicFieldData> extends IndexCompone
          * parent + 1, or 0 if there is no previous parent, and R (excluded).
          */
         public static class Nested {
-            private final BitDocIdSetFilter rootFilter, innerFilter;
 
-            public Nested(BitDocIdSetFilter rootFilter, BitDocIdSetFilter innerFilter) {
+            private final BitDocIdSetFilter rootFilter;
+            private final Filter innerFilter;
+
+            public Nested(BitDocIdSetFilter rootFilter, Filter innerFilter) {
                 this.rootFilter = rootFilter;
                 this.innerFilter = innerFilter;
             }
@@ -144,10 +146,10 @@ public interface IndexFieldData<FD extends AtomicFieldData> extends IndexCompone
             }
 
             /**
-             * Get a {@link BitDocIdSet} that matches the inner documents.
+             * Get a {@link DocIdSet} that matches the inner documents.
              */
-            public BitDocIdSet innerDocs(LeafReaderContext ctx) throws IOException {
-                return innerFilter.getDocIdSet(ctx);
+            public DocIdSet innerDocs(LeafReaderContext ctx) throws IOException {
+                return innerFilter.getDocIdSet(ctx, null);
             }
         }
 