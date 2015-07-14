bleskes/elasticsearch

Generify Index and Shard exceptions
Today we have a intermediate hierarchy for shard and index exceptions
which makes it hard to introduce generic exceptions like ResourceNotFoundException
intoduced in this commit. This commit breaks up the hierarchy by adding index and shard
as a special internal header that gets rendered for every exception that fills that header.
This commit removes dedicated exceptions like `IndexMissingException` or
`IndexShardMissingException` in favour of `ResourceNotFoundException`

core/src/main/java/org/elasticsearch/action/support/broadcast/BroadcastShardOperationFailedException.java
@@ -19,9 +19,9 @@
 
 package org.elasticsearch.action.support.broadcast;
 
+import org.elasticsearch.ElasticsearchException;
 import org.elasticsearch.ElasticsearchWrapperException;
 import org.elasticsearch.common.io.stream.StreamInput;
-import org.elasticsearch.index.shard.IndexShardException;
 import org.elasticsearch.index.shard.ShardId;
 
 import java.io.IOException;
@@ -31,18 +31,19 @@ import java.io.IOException;
  *
  *
  */
-public class BroadcastShardOperationFailedException extends IndexShardException implements ElasticsearchWrapperException {
+public class BroadcastShardOperationFailedException extends ElasticsearchException implements ElasticsearchWrapperException {
 
     public BroadcastShardOperationFailedException(ShardId shardId, String msg) {
-        super(shardId, msg, null);
+        this(shardId, msg, null);
     }
 
     public BroadcastShardOperationFailedException(ShardId shardId, Throwable cause) {
-        super(shardId, "", cause);
+        this(shardId, "", cause);
     }
 
     public BroadcastShardOperationFailedException(ShardId shardId, String msg, Throwable cause) {
-        super(shardId, msg, cause);
+        super(msg, cause);
+        addShard(shardId);
     }
 
     public BroadcastShardOperationFailedException(StreamInput in) throws IOException{

core/src/main/java/org/elasticsearch/index/snapshots/IndexShardRestoreException.java
@@ -19,8 +19,8 @@
 
 package org.elasticsearch.index.snapshots;
 
+import org.elasticsearch.ElasticsearchException;
 import org.elasticsearch.common.io.stream.StreamInput;
-import org.elasticsearch.index.shard.IndexShardException;
 import org.elasticsearch.index.shard.ShardId;
 
 import java.io.IOException;
@@ -28,13 +28,14 @@ import java.io.IOException;
 /**
  * Generic shard restore exception
  */
-public class IndexShardRestoreException extends IndexShardException {
+public class IndexShardRestoreException extends ElasticsearchException {
     public IndexShardRestoreException(ShardId shardId, String msg) {
-        super(shardId, msg);
+        this(shardId, msg, null);
     }
 
     public IndexShardRestoreException(ShardId shardId, String msg, Throwable cause) {
-        super(shardId, msg, cause);
+        super(msg, cause);
+        addShard(shardId);
     }
 
     public IndexShardRestoreException(StreamInput in) throws IOException{

core/src/main/java/org/elasticsearch/indices/IndexCreationException.java
@@ -19,19 +19,20 @@
 
 package org.elasticsearch.indices;
 
+import org.elasticsearch.ElasticsearchException;
 import org.elasticsearch.ElasticsearchWrapperException;
 import org.elasticsearch.common.io.stream.StreamInput;
 import org.elasticsearch.index.Index;
-import org.elasticsearch.index.IndexException;
 
 import java.io.IOException;
 
 /**
  */
-public class IndexCreationException extends IndexException implements ElasticsearchWrapperException {
+public class IndexCreationException extends ElasticsearchException implements ElasticsearchWrapperException {
 
     public IndexCreationException(Index index, Throwable cause) {
-        super(index, "failed to create index", cause);
+        super("failed to create index", cause);
+        addIndex(index);
     }
 
     public IndexCreationException(StreamInput in) throws IOException{

openeuler-mirror/poissonsearch-oss

core/src/main/java/org/elasticsearch/action/support/broadcast/BroadcastShardOperationFailedException.java
@@ -19,9 +19,9 @@
 
 package org.elasticsearch.action.support.broadcast;
 
+import org.elasticsearch.ElasticsearchException;
 import org.elasticsearch.ElasticsearchWrapperException;
 import org.elasticsearch.common.io.stream.StreamInput;
-import org.elasticsearch.index.shard.IndexShardException;
 import org.elasticsearch.index.shard.ShardId;
 
 import java.io.IOException;
@@ -31,18 +31,19 @@ import java.io.IOException;
  *
  *
  */
-public class BroadcastShardOperationFailedException extends IndexShardException implements ElasticsearchWrapperException {
+public class BroadcastShardOperationFailedException extends ElasticsearchException implements ElasticsearchWrapperException {
 
     public BroadcastShardOperationFailedException(ShardId shardId, String msg) {
-        super(shardId, msg, null);
+        this(shardId, msg, null);
     }
 
     public BroadcastShardOperationFailedException(ShardId shardId, Throwable cause) {
-        super(shardId, "", cause);
+        this(shardId, "", cause);
     }
 
     public BroadcastShardOperationFailedException(ShardId shardId, String msg, Throwable cause) {
-        super(shardId, msg, cause);
+        super(msg, cause);
+        setShard(shardId);
     }
 
     public BroadcastShardOperationFailedException(StreamInput in) throws IOException{

core/src/main/java/org/elasticsearch/index/snapshots/IndexShardRestoreException.java
@@ -19,8 +19,8 @@
 
 package org.elasticsearch.index.snapshots;
 
+import org.elasticsearch.ElasticsearchException;
 import org.elasticsearch.common.io.stream.StreamInput;
-import org.elasticsearch.index.shard.IndexShardException;
 import org.elasticsearch.index.shard.ShardId;
 
 import java.io.IOException;
@@ -28,13 +28,14 @@ import java.io.IOException;
 /**
  * Generic shard restore exception
  */
-public class IndexShardRestoreException extends IndexShardException {
+public class IndexShardRestoreException extends ElasticsearchException {
     public IndexShardRestoreException(ShardId shardId, String msg) {
-        super(shardId, msg);
+        this(shardId, msg, null);
     }
 
     public IndexShardRestoreException(ShardId shardId, String msg, Throwable cause) {
-        super(shardId, msg, cause);
+        super(msg, cause);
+        setShard(shardId);
     }
 
     public IndexShardRestoreException(StreamInput in) throws IOException{

core/src/main/java/org/elasticsearch/indices/IndexCreationException.java
@@ -19,19 +19,20 @@
 
 package org.elasticsearch.indices;
 
+import org.elasticsearch.ElasticsearchException;
 import org.elasticsearch.ElasticsearchWrapperException;
 import org.elasticsearch.common.io.stream.StreamInput;
 import org.elasticsearch.index.Index;
-import org.elasticsearch.index.IndexException;
 
 import java.io.IOException;
 
 /**
  */
-public class IndexCreationException extends IndexException implements ElasticsearchWrapperException {
+public class IndexCreationException extends ElasticsearchException implements ElasticsearchWrapperException {
 
     public IndexCreationException(Index index, Throwable cause) {
-        super(index, "failed to create index", cause);
+        super("failed to create index", cause);
+        setIndex(index);
     }
 
     public IndexCreationException(StreamInput in) throws IOException{