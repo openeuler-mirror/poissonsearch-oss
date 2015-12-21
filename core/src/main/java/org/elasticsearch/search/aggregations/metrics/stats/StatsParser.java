elastic/elasticsearch

Aggregations Refactor: Refactor Percentiles and Percentile Ranks Aggregation

core/src/main/java/org/elasticsearch/search/aggregations/metrics/stats/StatsParser.java
@@ -58,7 +58,7 @@ public class StatsParser extends NumericValuesSourceParser {
     }
 
     @Override
-    public AggregatorFactory getFactoryPrototype() {
-        return new StatsAggegator.Factory(null);
+    public AggregatorFactory[] getFactoryPrototypes() {
+        return new AggregatorFactory[] { new StatsAggegator.Factory(null) };
     }
 }

openeuler-mirror/poissonsearch-oss

core/src/main/java/org/elasticsearch/search/aggregations/metrics/stats/StatsParser.java
@@ -58,7 +58,7 @@ public class StatsParser extends NumericValuesSourceParser {
     }
 
     @Override
-    public AggregatorFactory getFactoryPrototype() {
-        return new StatsAggregator.Factory(null);
+    public AggregatorFactory[] getFactoryPrototypes() {
+        return new AggregatorFactory[] { new StatsAggregator.Factory(null) };
     }
 }